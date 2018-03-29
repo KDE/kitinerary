d1ff10a27886f8b0607edfce5b7fafca879a4f67
Volker Krause <vkrause@kde.org>
2018-03-29 22:33:24 +0200

    Add one more pkpass extractor
    Exposes a problem with JS date/time handling losing timezone information
    by always converting to local time, which is not what we need here.

---
+++ src/extractors/eurowings-pkpass.js
@@ -0,0 +1,41 @@
+/*
+   Copyright (c) 2018 Volker Krause <vkrause@kde.org>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+   License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+function main(pass)
+{
+    var res = JsonLd.newObject("FlightReservation");
+    res.airplaneSeat = pass.field["seat"].value;
+    res.reservationFor = JsonLd.newObject("Flight");
+    res.reservationFor.departureGate = pass.field["gate"].value;
+    res.reservationFor.departureAirport = JsonLd.newObject("Airport");
+    res.reservationFor.departureAirport.iataCode = pass.field["origin"].value;
+    res.reservationFor.departureAirport.name = pass.field["origin"].label;
+    res.reservationFor.arrivalAirport = JsonLd.newObject("Airport");
+    res.reservationFor.arrivalAirport.iataCode = pass.field["destination"].value;
+    res.reservationFor.arrivalAirport.name = pass.field["destination"].label;
+    res.reservationNumber = pass.field["recordlocator"].value;
+    res.reservationFor.boardingTime = JsonLd.toDateTime(pass.field["departureDate"].value + ' ' + pass.field["boarding"].value, "M/d/yyyy hh:mm", "en");
+
+    res.reservationFor.flightNumber = pass.field["flightNumber"].value.substring(3);
+    res.reservationFor.airline = JsonLd.newObject("Airline");
+    res.reservationFor.airline.iataCode = pass.field["flightNumber"].value.substring(0, 2);
+    res.reservationFor.airline.name = pass.field["operatingcarrier"].value;
+
+    return res;
+}
