e2bbad07175b8b2561e3ed147ba4a276046bb2f2
Volker Krause <vkrause@kde.org>
2018-04-02 17:37:03 +0200

    Add extractor for Swiss boarding passes
    They lack the "relevantDate" field, which prevents the generic extraction
    from determining the boarding time.

---
+++ src/extractors/swiss-pkpass.js
@@ -0,0 +1,37 @@
+/*
+   Copyright (c) 2018 Volker Krause <vkrause@kde.org>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+   License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+function main(pass)
+{
+    var res = JsonLd.newObject("FlightReservation");
+    res.reservationFor = JsonLd.newObject("Flight");
+    res.reservationFor.departureGate = pass.field["gate"].value;
+    res.reservationFor.departureAirport = JsonLd.newObject("Airport");
+    res.reservationFor.departureAirport.name = pass.field["depart"].label;
+    res.reservationFor.arrivalAirport = JsonLd.newObject("Airport");
+    res.reservationFor.arrivalAirport.name = pass.field["destination"].label;
+
+    var date = JsonLd.toDateTime(pass.field["date"].value + ' ' + Context.senderDate.getFullYear() + ' ' + pass.field["boarding"].value, "ddMMMM yyyy hh:mm", "en");
+    if (date < Context.senderDate)
+        date.setFullYear(Context.senderDate.getFullYear() + 1);
+    res.reservationFor.boardingTime = date;
+
+    return res;
+}
+
