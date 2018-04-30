8d9934cb47e2cd1fe7642cb56883aaf84ce5714b
Volker Krause <vkrause@kde.org>
2018-04-30 19:58:47 +0200

    Make the SNCF extractor handle a few more variations

--- src/extractors/sncf.js
+++ src/extractors/sncf.js
@@ -27,11 +27,11 @@ function parseDate(dateStr, timeStr) {
 
 function main(text) {
     var reservations = new Array();
-    var bookingRef = text.match(/DOSSIER VOYAGE : ([A-Z0-9]{6})/);
+    var bookingRef = text.match(/DOSSIER VOYAGE : +([A-Z0-9]{6})/);
 
     var pos = 0;
     while (true) {
-        var header = text.substr(pos).match(/  Départ \/ Arrivée.*\n/);
+        var header = text.substr(pos).match(/ +Départ \/ Arrivée.*\n/);
         if (!header)
             break;
         var index = header.index + header[0].length;
@@ -40,7 +40,7 @@ function main(text) {
         res.reservationNumber = bookingRef[1];
         res.reservationFor = JsonLd.newObject("TrainTrip");
 
-        var depLine = text.substr(pos + index).match(/\n  ([\w -]+?)  +(\d{2}\/\d{2}) à (\d{2}h\d{2})/);
+        var depLine = text.substr(pos + index).match(/\n {2,3}([\w -]+?)  +(\d{2}\/\d{2}) à (\d{2}h\d{2})/);
         if (!depLine)
             break;
         index += depLine.index + depLine[0].length;
@@ -48,7 +48,7 @@ function main(text) {
         res.reservationFor.departureStation.name = depLine[1];
         res.reservationFor.departureTime = parseDate(depLine[2],  depLine[3]);
 
-        var arrLine = text.substr(pos + index).match(/\n  ([\w -]+?)  +(\d{2}\/\d{2}) à (\d{2}h\d{2})/);
+        var arrLine = text.substr(pos + index).match(/\n {2,3}([\w -]+?)  +(\d{2}\/\d{2}) à (\d{2}h\d{2})/);
         if (!arrLine)
             break;
         index += arrLine.index + arrLine[0].length;
@@ -60,15 +60,15 @@ function main(text) {
         // since the stations are vertically centered, the stuff we are looking for might be at different
         // positions relative to them
         var legText = text.substring(pos + header.index + header[0].length, pos + index);
-        var trainNumber = legText.match(/TRAIN N°(\d{3,4})/);
+        var trainNumber = legText.match(/TRAIN N° ?(\d{3,4})/);
         if (trainNumber)
             res.reservationFor.trainNumber = trainNumber[1];
-        var seatRes = legText.match(/VOITURE (\d+) - PLACE (\d+)/);
+        var seatRes = legText.match(/(VOITURE|COACH) (\d+) - PLACE (\d+)/);
         if (seatRes) {
             res.reservedTicket = JsonLd.newObject("Ticket");
             res.reservedTicket.ticketedSeat = JsonLd.newObject("Seat");
-            res.reservedTicket.ticketedSeat.seatSection = seatRes[1];
-            res.reservedTicket.ticketedSeat.seatNumber = seatRes[2];
+            res.reservedTicket.ticketedSeat.seatSection = seatRes[2];
+            res.reservedTicket.ticketedSeat.seatNumber = seatRes[3];
         }
 
         reservations.push(res);
