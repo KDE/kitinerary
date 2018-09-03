b3039482590df1036dba49875979c02a32b5daeb
Volker Krause <vkrause@kde.org>
2018-09-03 19:16:20 +0200

    Fix copyright year

--- src/extractors/korail.js
+++ src/extractors/korail.js
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2017 Volker Krause <vkrause@kde.org>
+   Copyright (c) 2018 Volker Krause <vkrause@kde.org>
 
    This library is free software; you can redistribute it and/or modify it
    under the terms of the GNU Library General Public License as published by
