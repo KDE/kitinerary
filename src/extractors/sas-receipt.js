73bdf0196b8649789ebfe2bafdf8135e66b83e89
Volker Krause <vkrause@kde.org>
2018-09-12 18:22:37 +0200

    Fix extracting the airline IATA code in SAS booking receips

--- src/extractors/sas-receipt.js
+++ src/extractors/sas-receipt.js
@@ -32,7 +32,7 @@ function extractInformation(page) {
         var res = new Array();
         var lines = text.split("\n");
         var lastFlight = null;
-        var bookingRef = text.match(/Booking Reference:\s* ([A-Z0-9]+)\s+IATA Number:\s+([0-9]+)/);
+        var bookingRef = text.match(/Booking Reference:\s* ([A-Z0-9]+)\s/);
         var issueDate = text.match(/.*Date of Issue:\s([0-9A-Z]+)/);
         var state = 0;
         var operatedBy = "";
@@ -58,22 +58,22 @@ function extractInformation(page) {
                 //  LH 160 / 27DEC Frankfurt FRA - Leipzig/Halle                             17:00    17:55                 Terminal 1       1PC
                 //  SK 1674 / 03JAN Berlin Tegel - Copenhagen Kastrup                        13:30    14:30         12:30                    1PC
                 //  SK 675 / 27DEC Copenhagen Kastrup - Frankfurt FRA                        13:40    15:15         12:40 Terminal 3         1PC
-                var flightLine = lines[i].match(/([A-Z]+ [0-9]+) \/ ([0-9]{2}[A-Z]{3})\s+([^0-9]+)([0-9]{2}:[0-9]{2})\s+([0-9]{2}:[0-9]{2})\s+([0-9]{2}:[0-9]{2})?.*/)
+                var flightLine = lines[i].match(/([A-Z0-9]{2}) ([0-9]{1,4}) \/ ([0-9]{2}[A-Z]{3})\s+([^0-9]+)([0-9]{2}:[0-9]{2})\s+([0-9]{2}:[0-9]{2})\s+([0-9]{2}:[0-9]{2})?.*/)
                 if (flightLine) {
                     var flight = JsonLd.newObject("FlightReservation");
                     flight.reservationNumber = bookingRef[1];
                     flight.reservationFor = JsonLd.newObject("Flight");
-                    flight.reservationFor.flightNumber = flightLine[1];
+                    flight.reservationFor.flightNumber = flightLine[2];
                     flight.reservationFor.airline = JsonLd.newObject("Airline");
-                    flight.reservationFor.airline.iataCode = bookingRef[2];
+                    flight.reservationFor.airline.iataCode = flightLine[1];
                     flight.reservationFor.airline.name = operatedBy;
-                    flight.reservationFor.departureTime = parseDate(flightLine[2], flightLine[4], issueDate[1] );
-                    var airports = flightLine[3].split(" - ");
+                    flight.reservationFor.departureTime = parseDate(flightLine[3], flightLine[5], issueDate[1] );
+                    var airports = flightLine[4].split(" - ");
                     flight.reservationFor.departureAirport = JsonLd.newObject("Airport");
                     flight.reservationFor.departureAirport.name = airports[0].trim();;
-                    flight.reservationFor.departureTime = parseDate(flightLine[2], flightLine[5], issueDate[1] );
+                    flight.reservationFor.arrivalTime = parseDate(flightLine[3], flightLine[6], issueDate[1] );
                     flight.reservationFor.arrivalAirport = JsonLd.newObject("Airport");
-                    flight.reservationFor.arrivalAirport.name = airports[1].trim();;
+                    flight.reservationFor.arrivalAirport.name = airports[1].trim();
                     res.push(flight);
                 }
                 state = 4;
