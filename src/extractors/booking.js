15bb007d39dc2fd1c8aa74a229cccb36aa2229e4
Volker Krause <vkrause@kde.org>
2019-01-18 17:54:37 +0100

    Improve phone number extraction in booking.com emails

--- src/extractors/booking.js
+++ src/extractors/booking.js
@@ -21,7 +21,7 @@ var regExMap = [];
 regExMap['en_US'] = [];
 regExMap['en_US']['bookingRef'] = /Booking number +([0-9]*)\s+/;
 // 1: adress, 2: city, 3:postal code, 4: country, 5: phone
-regExMap['en_US']['hotelInformation'] = / *(.+), (.+), (.+), (.+) -\s+Phone: (\+[0-9]*)\s+/;
+regExMap['en_US']['hotelInformation'] = / *(.+), (.+), (.+), (.+) -\s+Phone: (\+[0-9 ]*)\s+/;
 regExMap['en_US']['hotelName'] = /\[checkmark\.png\] (.*) is expecting you on/;
 regExMap['en_US']['arrivalDate'] = /Check-in *([A-z]+ [0-9]{1,2} [A-z]+ [0-9]+) \(f?r?o?m? ?([0-9]{1,2}:[0-9]{2})[^\)]*\)/;
 regExMap['en_US']['departureDate'] = /Check-out *([1-z]+ [0-9]{1,2} [A-z]+ [0-9]+) \(.* ([0-9]{1,2}:[0-9]{2})\)/;
