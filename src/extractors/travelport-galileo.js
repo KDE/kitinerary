3c4c3c22d1f1a02dfd631e01a9174caafcc76f06
Volker Krause <vkrause@kde.org>
2019-07-01 18:49:22 +0200

    Add extractor for Travelport Galileo reservations
    Despite being a backend system, like with Amadeus this is nevertheless
    exposed to consumers regularly.

---
+++ src/extractors/travelport-galileo.js
@@ -0,0 +1,64 @@
+/*
+   Copyright (c) 2019 Volker Krause <vkrause@kde.org>
+
+   This library is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or (at your
+   option) any later version.
+
+   This library is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+   License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+*/
+
+function parsePNR(text) {
+    var reservations = new Array();
+
+    var passengers = new Array();
+    var pos = 0;
+    while (true) {
+        var passenger = text.substr(pos).match(/\d\.1(.+?)\/([\w\s]+?)(?:\*|  |\n)/);
+        if (!passenger)
+            break;
+        pos += passenger.index + passenger[0].length;
+
+        var person = JsonLd.newObject("Person");
+        person.familyName = passenger[1];
+        person.givenName = passenger[2];
+        passengers.push(person);
+    }
+
+    pos = 0;
+    var legs = new Array();
+    while (true) {
+        var leg = text.substr(pos).match(/\d. ([A-Z0-9]{2}) +(\d{1,4}) [A-Z]{1,2} {1,2}(\d{2}[A-Z]{3}) ([A-Z]{3})([A-Z]{3}) [A-Z]{2}\d  (\d{4})  [ #](\d{4}).*\n(?:\s*OPERATED BY (.*?)\n)?/);
+        if (!leg)
+            break;
+        pos += leg.index + leg[0].length;
+
+        var res = JsonLd.newFlightReservation();
+        res.reservationFor.airline.iataCode = leg[1];
+        res.reservationFor.airline.name = leg[8];
+        res.reservationFor.flightNumber = leg[2];
+        res.reservationFor.departureAirport.iataCode = leg[4];
+        res.reservationFor.arrivalAirport.iataCode = leg[5];
+        res.reservationFor.departureTime = JsonLd.toDateTime(leg[3] + leg[6], "ddMMMhhmm", "en");
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(leg[3] + leg[7], "ddMMMhhmm", "en");
+        legs.push(res);
+    }
+
+    for (var i = 0; i < legs.length; ++i) {
+        for (var j = 0; j < passengers.length; ++j) {
+            var res = JsonLd.clone(legs[i]);
+            res.underName = passengers[j];
+            reservations.push(res);
+        }
+    }
+    return reservations;
+}
