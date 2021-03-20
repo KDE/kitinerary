517230ac5935f08c72668dd904c08e54c298d20b
Volker Krause <vkrause@kde.org>
2021-03-20 19:59:50 +0100

    Add abstract base classes for extractors
    The current script extractors will be rebased onto this, while also
    allowing built-in C++ extractors next to them using the same interface.
    This will replace the current generic/custom extractor split.

---
+++ src/lib/engine/abstractextractor.cpp
@@ -0,0 +1,11 @@
+/*
+    SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "abstractextractor.h"
+
+using namespace KItinerary;
+
+AbstractExtractor::~AbstractExtractor() = default;
---
+++ src/lib/engine/abstractextractor.h
@@ -0,0 +1,39 @@
+/*
+    SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef KITINERARY_ABSTRACTEXTRACTOR_H
+#define KITINERARY_ABSTRACTEXTRACTOR_H
+
+#include "kitinerary_export.h"
+
+class QString;
+
+namespace KItinerary {
+class ExtractorDocumentNode;
+class ExtractorEngine;
+class ExtractorResult;
+
+/** Abstract base class for data extractors. */
+class KITINERARY_EXPORT AbstractExtractor
+{
+public:
+    virtual ~AbstractExtractor();
+
+    /** Identifier for this extractor.
+     *  Mainly used for diagnostics and tooling.
+     */
+    virtual QString name() const = 0;
+
+    /** Fast check whether this extractor is applicable for @p node. */
+    virtual bool canHandle(const ExtractorDocumentNode &node) const = 0;
+
+    /** Extract data from @p node. */
+    virtual ExtractorResult extract(const ExtractorDocumentNode &node, const ExtractorEngine *engine) const = 0;
+};
+
+}
+
+#endif // KITINERARY_ABSTRACTEXTRACTOR_H
