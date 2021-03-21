3153388a2e36b2d0611c1f25707c1314b21ee636
Volker Krause <vkrause@kde.org>
2021-03-21 16:06:09 +0100

    Add automatic test for the extractor script environment
    This validates the right arguments are passed for all the different input
    types, something we previously could only test implicitly by relying on
    the big (non-public) extractor test suite.

---
+++ autotests/extractorscriptenginetest.cpp
@@ -0,0 +1,87 @@
+/*
+    SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include <KItinerary/ExtractorDocumentNode>
+#include <KItinerary/ExtractorDocumentNodeFactory>
+#include <KItinerary/ExtractorDocumentProcessor>
+#include <KItinerary/ExtractorEngine>
+#include <KItinerary/ExtractorResult>
+#include <KItinerary/ScriptExtractor>
+
+#include <QDebug>
+#include <QDirIterator>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QTest>
+
+using namespace KItinerary;
+
+#define s(x) QStringLiteral(x)
+
+static void expandRecursive(ExtractorDocumentNode &node, const ExtractorEngine *engine)
+{
+    if (node.isNull()) {
+        return;
+    }
+    node.processor()->expandNode(node, engine);
+    for (auto child : node.childNodes()) {
+        expandRecursive(child, engine);
+    }
+
+    node.processor()->preExtract(node, engine);
+}
+
+class ExtractorScriptEngineTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void testArguments_data()
+    {
+        QTest::addColumn<QString>("inputFile");
+        QTest::addColumn<QString>("refFile");
+
+        QTest::newRow("text/plain") << s(SOURCE_DIR "/scriptenginedata/plain-text.txt") << s(SOURCE_DIR "/scriptenginedata/plain-text.txt.json");
+        QTest::newRow("pkpass") << s(SOURCE_DIR "/pkpassdata/swiss.pkpass") << s(SOURCE_DIR "/scriptenginedata/swiss.pkpass.json");
+        QTest::newRow("IATA BCBP PDF") << s(SOURCE_DIR "/extractordata/synthetic/iata-bcbp-demo.pdf") << s(SOURCE_DIR "/scriptenginedata/iata-bcbp-demo.pdf.json");
+        QTest::newRow("ical") << s(SOURCE_DIR "/extractordata/ical/eventreservation.ics") << s(SOURCE_DIR "/scriptenginedata/eventreservation.ics.json");
+        QTest::newRow("uic9183") << s(SOURCE_DIR "/uic918-3/valid/Testw_VOW8XG6_A9E8DXL_0.bin") << s(SOURCE_DIR "/scriptenginedata/Testw_VOW8XG6_A9E8DXL_0.bin.json");
+        QTest::newRow("html") << s(SOURCE_DIR "/structureddata/google-flight-reservation-json-ld.html") << s(SOURCE_DIR "/scriptenginedata/google-flight-reservation-json-ld.html.json");
+    }
+
+    void testArguments()
+    {
+        QFETCH(QString, inputFile);
+        QFETCH(QString, refFile);
+
+        QFile in(inputFile);
+        QVERIFY(in.open(QFile::ReadOnly));
+
+        ExtractorEngine engine;
+        auto root = engine.documentNodeFactory()->createNode(in.readAll(), inputFile);
+        QVERIFY(!root.isNull());
+        expandRecursive(root, &engine);
+
+        ScriptExtractor extractor;
+        extractor.setScriptFileName(s(":/reflector.js"));
+        extractor.setScriptFunction(s("dumpArgs"));
+        const auto result = extractor.extract(root, &engine).jsonLdResult();
+
+        QFile ref(refFile);
+        QVERIFY(ref.open(QFile::ReadOnly));
+        const auto refResult = QJsonDocument::fromJson(ref.readAll()).array();
+
+        if (result != refResult) {
+            qDebug().noquote() << QJsonDocument(result).toJson();
+        }
+        QCOMPARE(result, refResult);
+    }
+};
+
+QTEST_GUILESS_MAIN(ExtractorScriptEngineTest)
+
+#include "extractorscriptenginetest.moc"
---
+++ autotests/reflector.js
@@ -0,0 +1,57 @@
+/*
+    SPDX-FileCopyrightText: 2021 Volker Krause <vkrause@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+function dumpValue(value, depth)
+{
+    if (value === undefined || value === null
+        || typeof value == "boolean"
+        || typeof value == "number"
+        || typeof value == "string")
+    {
+        return value;
+    } else if (typeof value == "object") {
+        if (depth == 0) {
+            return "[...]";
+        }
+        if (value.length != undefined) {
+            var arr = new Array();
+            for (var i = 0; i < value.length; ++i) {
+                arr.push(dumpValue(value[i], depth - 1));
+            }
+            return arr;
+        }
+        var obj = new Object();
+        var empty = true;
+        for (var prop in value) {
+            if (prop == "parent" || prop == "firstChild" || prop == "nextSibling" || value[prop] === undefined || value[prop] === null || typeof value[prop] == "function") {
+                continue;
+            }
+            obj[prop] = dumpValue(value[prop], depth - 1);
+            empty = false;
+        }
+
+        // implicitly convertible (color, date, etC)
+        if (empty) {
+            return value.toString();
+        } else {
+            return obj;
+        }
+    } else {
+        console.log("unhandled value type", typeof value);
+    }
+    return value;
+}
+
+function dumpArgs(content, node)
+{
+    var res = new Array();
+    var wrapper = new Object();
+    wrapper.content = dumpValue(content, 6);
+    res.push(wrapper);
+    res.push(dumpValue(node, 6));
+    res.push(dumpValue(Context, 6));
+    return res;
+}
