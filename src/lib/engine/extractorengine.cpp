8f90c123d504e2ae1d4db6310da77f3b9e49f5c1
Volker Krause <vkrause@kde.org>
2021-03-31 18:10:05 +0200

    Create document nodes by default
    We don't take the result from there yet (although it meanwhile passes the
    full test suite as well), but this enables the migration of KItinerary
    Workbench to the new API.

--- src/lib/engine/extractorengine.cpp
+++ src/lib/engine/extractorengine.cpp
@@ -520,8 +520,14 @@ void ExtractorEngine::setContextDate(const QDateTime &dt)
 
 QJsonArray ExtractorEngine::extract()
 {
+    d->m_rootNode.setParent(d->m_contextNode);
+    d->processNode(d->m_rootNode);
+#if 0
+    return d->m_rootNode.result().jsonLdResult();
+#else
     d->extractDocument();
     return d->m_result;
+#endif
 }
 
 void ExtractorEnginePrivate::extractRecursive(KMime::Content *content)
