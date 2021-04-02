640560cbb2ce3a930a4493fede34f40c3a056dfa
Volker Krause <vkrause@kde.org>
2021-04-02 13:46:17 +0200

    Remove unit test for the old API
    This one will break once we change the extractor script metadata to use
    the new more detailed type information.

--- autotests/extractorrepositorytest.cpp
+++ autotests/extractorrepositorytest.cpp
@@ -5,7 +5,6 @@
 */
 
 #include <KItinerary/AbstractExtractor>
-#include <KItinerary/Extractor>
 #include <KItinerary/ExtractorDocumentNode>
 #include <KItinerary/ExtractorDocumentNodeFactory>
 #include <KItinerary/ExtractorEngine>
@@ -37,21 +36,6 @@ private Q_SLOTS:
         QVERIFY(repo.extractorByName(u"<IATA BCBP>"));
         QVERIFY(repo.extractorByName(u"chaos-communication-congress"));
         QVERIFY(!repo.extractorByName(u"I-DONT-EXIST"));
-    }
-
-    void testApplyFilter()
-    {
-        ExtractorRepository repo;
-
-        std::vector<Extractor> extractors;
-        repo.extractorsForBarcode(QStringLiteral("i0CVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxX"), extractors);
-        QCOMPARE(extractors.size(), 1);
-        QVERIFY(extractors[0].name().startsWith(QLatin1String("sncf")));
-
-        extractors.clear();
-        repo.extractorsForContent(QStringLiteral("PNR:1234567890,TRAIN:12345,DOJ:dd-mm-yyyy,TIME:hh:mm,SL,A TO B,John Doe+2,S7 49 S7 52 S7 55,FARE:140,SC:10+PG CHGS."), extractors);
-        QCOMPARE(extractors.size(), 1);
-        QVERIFY(extractors[0].name().startsWith(QLatin1String("irctc")));
     }
 
     void testExtractorsForNode()
