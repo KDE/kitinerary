ece5df2b133fab8fc7593ee5c27fb1d5ead6c3c4
Volker Krause <vkrause@kde.org>
2021-05-06 17:44:54 +0200

    Handle more variants of NH hotel booking confirmation emails

--- src/lib/extractors/nh-hotels.js
+++ src/lib/extractors/nh-hotels.js
@@ -12,6 +12,11 @@ function lastChild(elem) {
     return child;
 }
 
+function parseDateTime(text) {
+    var dt = text.match(/([\d/]+).*?([\d:]+)/);
+    return JsonLd.toDateTime(dt[1] + dt[2], "dd/MM/yyyyhh:mm", "en");
+}
+
 function parseHtml(doc) {
 
     var res = JsonLd.newObject("LodgingReservation");
@@ -38,22 +43,27 @@ function parseHtml(doc) {
     }
 
     var bookingRef = doc.eval("//table//tr/td/table//tr/td[2]")[0];
-    res.reservationNumber = bookingRef.content;
-    var bookingRow = bookingRef.parent.nextSibling.nextSibling;
+    console.log(bookingRef.recursiveContent)
+    if (bookingRef.content) {
+        res.reservationNumber = bookingRef.content;
+        var bookingRow = bookingRef.parent.nextSibling.nextSibling;
 
-    res.underName = JsonLd.newObject("Person");
-    res.underName.name = bookingRow.firstChild.nextSibling.content;
-    bookingRow = bookingRow.nextSibling.nextSibling;
+        res.underName = JsonLd.newObject("Person");
+        res.underName.name = bookingRow.firstChild.nextSibling.content;
+        bookingRow = bookingRow.nextSibling.nextSibling;
 
-    res.underName.email = bookingRow.firstChild.nextSibling.recursiveContent;
-    bookingRow = bookingRow.nextSibling.nextSibling.nextSibling;
+        res.underName.email = bookingRow.firstChild.nextSibling.recursiveContent;
+        bookingRow = bookingRow.nextSibling.nextSibling.nextSibling;
 
-    var dt = bookingRow.firstChild.nextSibling.content.match(/([\d/]+).*?([\d:]+)/);
-    res.checkinTime = JsonLd.toDateTime(dt[1] + dt[2], "dd/MM/yyyyhh:mm", "en");
-    bookingRow = bookingRow.nextSibling.nextSibling;
+        res.checkinTime = parseDateTime(bookingRow.firstChild.nextSibling.content);
+        bookingRow = bookingRow.nextSibling.nextSibling;
+        res.checkoutTime = parseDateTime(bookingRow.firstChild.nextSibling.content);
+    } else {
+        res.reservationNumber = doc.eval("//td[@class='table-confirmation text-bold']/a")[0].content;
 
-    dt = bookingRow.firstChild.nextSibling.content.match(/([\d/]+).*?([\d:]+)/);
-    res.checkoutTime = JsonLd.toDateTime(dt[1] + dt[2], "dd/MM/yyyyhh:mm", "en");
-
+        dts = doc.eval("//td[2]/strong");
+        res.checkinTime = parseDateTime(dts[0].content);
+        res.checkoutTime = parseDateTime(dts[1].content);
+    }
     return res;
 }
