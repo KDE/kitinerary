5c951c5451ff6bd23854c594b95e0325297942e6
Volker Krause <vkrause@kde.org>
2021-05-18 18:05:04 +0200

    Extend BCBP unit tests to cover the new IATA BCBP types
    This also found a few typos in our test data that the old code
    had not noticed so far.

--- autotests/bcbpparsertest.cpp
+++ autotests/bcbpparsertest.cpp
@@ -17,6 +17,8 @@
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QObject>
+#include <QProcess>
+#include <QStandardPaths>
 #include <QTest>
 
 using namespace KItinerary;
@@ -31,27 +33,27 @@ private Q_SLOTS:
         QTest::addColumn<QString>("refFile");
 
         // example data from IATA resolution 792 version 5 Attachment B (with security sections shortended or omitted)
-        QTest::newRow("single leg, mandatory only") << QStringLiteral("M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100") << QStringLiteral("iata-resolution792-example1.json");
-        QTest::newRow("single leg, all fields") << QStringLiteral("M1DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58Z^108ABCDEFGH") << QStringLiteral("iata-resolution792-example2.json");
-        QTest::newRow("single leg, partial") << QStringLiteral("M1GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    Y^18ABCDEFGH") << QStringLiteral("iata-resolution792-example3.json");
-        QTest::newRow("multi leg, all fields") << QStringLiteral("M2DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58ZDEF456 FRAGVALH 3664 327C012C0002 12E2A0140987654321 1AC AC 1234567890123    3PCNWQ^108ABCDEFGH") << QStringLiteral("iata-resolution792-example4.json");
-        QTest::newRow("multi leg, partial") << QStringLiteral("M2GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    YDEF456 CDGDTWNW 0049 339F001A0002 12C2A012098765432101                       2PC ^18ABCDEFGH") << QStringLiteral("iata-resolution792-example5.json");
+        QTest::newRow("single leg, mandatory only") << QStringLiteral("M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 326J001A0025 100") << QStringLiteral("iata-resolution792-example1");
+        QTest::newRow("single leg, all fields") << QStringLiteral("M1DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58Z^108ABCDEFGH") << QStringLiteral("iata-resolution792-example2");
+        QTest::newRow("single leg, partial") << QStringLiteral("M1GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    Y^108ABCDEFGH") << QStringLiteral("iata-resolution792-example3");
+        QTest::newRow("multi leg, all fields") << QStringLiteral("M2DESMARAIS/LUC       EAB12C3 YULFRAAC 0834 326J003A0027 167>5321WW1325BAC 0014123456002001412346700100141234789012A0141234567890 1AC AC 1234567890123    4PCYLX58ZDEF456 FRAGVALH 3664 327C012C0002 12E2A0140987654321 1AC AC 1234567890123    3PCNWQ^108ABCDEFGH") << QStringLiteral("iata-resolution792-example4");
+        QTest::newRow("multi leg, partial") << QStringLiteral("M2GRANDMAIRE/MELANIE  EABC123 GVACDGAF 0123 339C002F0025 130>5002A0571234567890  AF AF 1234567890123456    YDEF456 CDGDTWNW 0049 339F001A0002 12C2A012098765432101                       2PC ^108ABCDEFGH") << QStringLiteral("iata-resolution792-example5");
 
         // EW misses the 'E' eticket marker (BCBP item 253)
-        QTest::newRow("missing eticket indicator") << QStringLiteral("M1DOE/JOHN             XXX007 BRUTXLEW 8103 035Y012C0030 147>1181W 8033BEW 0000000000000291040000000000 0   LH 123456789012345     ") << QStringLiteral("missing-eticket-indicator.json");
+        QTest::newRow("missing eticket indicator") << QStringLiteral("M1DOE/JOHN             XXX007 BRUTXLEW 8103 035Y012C0030 147>1181W 8033BEW 0000000000000291040000000000 0   LH 123456789012345     ") << QStringLiteral("missing-eticket-indicator");
 
         // boarding pass issue date (BCBP item 22)
-        QTest::newRow("issue date") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2588 034Y023D0999 35D>5181WM7034BSN              2A08200000000000 SN LH 123456789012345      *30600000K0902       ") << QStringLiteral("issue-date.json");
+        QTest::newRow("issue date") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2588 034Y023D0999 35D>5181WM7034BSN              2A08200000000000 SN LH 123456789012345      *30600000K0902       ") << QStringLiteral("issue-date");
 
         // EasyJet being easy on the standard interpretation
-        QTest::newRow("easyjet") << QStringLiteral("M1DOE/JOHN            EABCDEFGMRSLGWEZY8724 99  3C  506  10Axxxxxxxxxx") << QStringLiteral("easyjet.json");
+        QTest::newRow("easyjet") << QStringLiteral("M1DOE/JOHN            EABCDEFGMRSLGWEZY8724 99  3C  506  10Axxxxxxxxxx") << QStringLiteral("easyjet");
 
         // Brussels Airlines short codes on booking confirmation
-        QTest::newRow("minimal1") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110Y") << QStringLiteral("minimal.json");
-        QTest::newRow("minimal2") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110") << QStringLiteral("minimal2.json");
+        QTest::newRow("minimal1") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110Y") << QStringLiteral("minimal");
+        QTest::newRow("minimal2") << QStringLiteral("M1DOE/JOHN            EXXX007 TXLBRUSN 2592 110") << QStringLiteral("minimal2");
 
         // TAP missing boarding pass issue date
-        QTest::newRow("no issue date") << QStringLiteral("M1DOE/JOHN            EXXX007 LISLCGTP 1080 204Y002D0003 35C>2180      B1A              2904712345678900                           *306      09     BRND") << QStringLiteral("tap-missing-issue-date.json");
+        QTest::newRow("no issue date") << QStringLiteral("M1DOE/JOHN            EXXX007 LISLCGTP 1080 204Y002D0003 35C>2180      B1A              2904712345678900                           *306      09     BRND") << QStringLiteral("tap-missing-issue-date");
 
     }
 
@@ -60,7 +62,35 @@ private Q_SLOTS:
         QFETCH(QString, message);
         QFETCH(QString, refFile);
 
-        QFile f(QStringLiteral(SOURCE_DIR "/bcbpdata/") + refFile);
+        QVERIFY(IataBcbp::maybeIataBcbp(message));
+        QVERIFY(IataBcbp::maybeIataBcbp(message.toUtf8()));
+        IataBcbp bcbp(message);
+        QVERIFY(bcbp.isValid());
+
+        QFile ref(QLatin1String(SOURCE_DIR "/bcbpdata/") + refFile + QLatin1String(".txt"));
+        QVERIFY(ref.open(QFile::ReadOnly));
+        const auto refOut = ref.readAll();
+
+        const auto exe = QStandardPaths::findExecutable(QStringLiteral("ticket-barcode-dump"), {QCoreApplication::applicationDirPath()});
+        QVERIFY(!exe.isEmpty());
+        QProcess proc;
+        proc.setProcessChannelMode(QProcess::ForwardedErrorChannel);
+        proc.setProgram(exe);
+        proc.setArguments({ QLatin1String("--context-date"), QDate(2018, 4, 2).toString(Qt::ISODate) });
+        proc.start();
+        QVERIFY(proc.waitForStarted());
+        proc.write(message.toUtf8());
+        QVERIFY(proc.waitForBytesWritten());
+        proc.closeWriteChannel();
+        QVERIFY(proc.waitForFinished());
+        const auto ticketDump = proc.readAllStandardOutput();
+        QCOMPARE(proc.exitCode(), 0);
+        if (ticketDump != refOut) {
+            qDebug().noquote() << ticketDump;
+        }
+        QVERIFY(ticketDump == refOut);
+
+        QFile f(QLatin1String(SOURCE_DIR "/bcbpdata/") + refFile + QLatin1String(".json"));
         QVERIFY(f.open(QFile::ReadOnly));
         const auto refArray = QJsonDocument::fromJson(f.readAll()).array();
         QVERIFY(!refArray.isEmpty());
@@ -72,9 +102,6 @@ private Q_SLOTS:
             qWarning().noquote() << QJsonDocument(resJson).toJson();
         }
         QCOMPARE(resJson, refArray);
-
-        IataBcbp bcbp(message);
-        QVERIFY(bcbp.isValid());
     }
 
     void testParserInvalid_data()
@@ -102,6 +129,6 @@ private Q_SLOTS:
     }
 };
 
-QTEST_APPLESS_MAIN(BcbpParserTest)
+QTEST_GUILESS_MAIN(BcbpParserTest)
 
 #include "bcbpparsertest.moc"
