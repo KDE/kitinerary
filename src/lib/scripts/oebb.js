d964441a0c5a267512562c72a35660a548f0efea
Volker Krause <vkrause@kde.org>
2022-02-27 13:27:56 +0100

    Improve extraction of ÖBB station names
    Those can be truncated in the RCT2 ticket data, so use what we find in the
    PDF as well.

--- src/lib/scripts/oebb.js
+++ src/lib/scripts/oebb.js
@@ -1,6 +1,5 @@
 /*
-   SPDX-FileCopyrightText: 2019 Volker Krause <vkrause@kde.org>
-
+   SPDX-FileCopyrightText: 2019-2022 Volker Krause <vkrause@kde.org>
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
@@ -11,10 +10,35 @@ function parseTicket(ticket, node) {
     var res = node.result[0];
 
     // decode 118199 vendor block
-    var block = ticket.block("118199");
-    var json = JSON.parse(block.contentText);
+    const block = ticket.block("118199");
+    const json = JSON.parse(block.contentText);
     if (!res.reservationFor.trainNumber)
         res.reservationFor.trainNumber = json["Z"];
 
     return res;
+}
+
+function parsePage(pdf, node, triggerNode) {
+    if (triggerNode.parent.result.length > 2) // not sure this can happen
+        return;
+
+    // look for non-elided station names
+    const text = pdf.pages[triggerNode.location].text;
+    const legs = text.match(/VON +-> NACH.*\n.*? ([A-Z].*) +-> (.*?)  .*\n.*? ([\w\*].*) +-> (.*?)  /);
+    console.log(legs);
+    if (!legs) { return triggerNode.result; }
+
+    var reservations = [];
+    var res = triggerNode.parent.result[0];
+    res.reservationFor.departureStation.name = legs[1];
+    res.reservationFor.arrivalStation.name = legs[2];
+    reservations.push(res);
+    if (triggerNode.parent.result.length == 2) {
+        var res = triggerNode.parent.result[1];
+        res.reservationFor.departureStation.name = legs[3];
+        res.reservationFor.arrivalStation.name = legs[4];
+        reservations.push(res);
+    }
+
+    return reservations;
 }
