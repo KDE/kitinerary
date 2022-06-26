3c970ee787c88857a79789e01f73059fa8ca0ef6
Volker Krause <vkrause@kde.org>
2022-06-26 08:43:37 +0200

    Add GWR ical extractor script

---
+++ src/lib/scripts/gwr.js
@@ -0,0 +1,17 @@
+/*
+   SPDX-FileCopyrightText: 2022 Volker Krause <vkrause@kde.org>
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+function parseEvent(event) {
+    let res = JsonLd.newTrainReservation();
+    res.reservationFor.departureTime = JsonLd.readQDateTime(event, 'dtStart');
+    res.reservationFor.arrivalTime = JsonLd.readQDateTime(event, 'dtEnd');
+    const trip = event.description.match(/Journey Details: (.*) \(([A-Z]{3})\) to (.*) \(([A-Z]{3})\)/);
+    res.reservationFor.departureStation.name = trip[1];
+    res.reservationFor.departureStation.identifier = 'uk:' + trip[2];
+    res.reservationFor.arrivalStation.name = trip[3];
+    res.reservationFor.arrivalStation.identifier = 'uk:' + trip[4];
+    res.reservationNumber = event.description.match(/Booking Reference: (.*)\n/)[1];
+    return res;
+}
