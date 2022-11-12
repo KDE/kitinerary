6b9f1547e4b0d0feb758f7f3282c3f7e970f184e
Volker Krause <vkrause@kde.org>
2022-11-12 12:59:15 +0100

    Attempt to fix the unity build on binary factory

--- src/lib/asn1/uperelement.h
+++ src/lib/asn1/uperelement.h
@@ -11,7 +11,7 @@
 namespace KItinerary {
 
 // The property counter thing is based on the approach described here https://woboq.com/blog/verdigris-implementation-tricks.html
-namespace detail {
+namespace uper_detail {
 template <int N = 255> struct num : public num<N - 1> {
     static constexpr int value = N;
     static constexpr num<N - 1> prev() { return {}; }
@@ -22,13 +22,13 @@ template <> struct num<0> { static constexpr int value = 0; };
 // start of an ASN.1 SEQUENCE definition
 #define UPER_GADGET \
     Q_GADGET \
-    static constexpr detail::num<0> _uper_optional_counter(detail::num<0>) { return {}; } \
+    static constexpr uper_detail::num<0> _uper_optional_counter(uper_detail::num<0>) { return {}; } \
     static constexpr bool _uper_ExtensionMarker = false;
 
 // same as UPER_GADGET, for SEQUENCES with an extension marker
 #define UPER_EXTENDABLE_GADGET \
     Q_GADGET \
-    static constexpr detail::num<0> _uper_optional_counter(detail::num<0>) { return {}; } \
+    static constexpr uper_detail::num<0> _uper_optional_counter(uper_detail::num<0>) { return {}; } \
     static constexpr bool _uper_ExtensionMarker = true;
 
 // ASN.1 ENUMERATED definitions, with or without extension marker
@@ -50,8 +50,8 @@ public: \
     Q_PROPERTY(Type Name MEMBER Name CONSTANT) \
     Q_PROPERTY(bool Name ## IsSet READ Name ## IsSet) \
 private: \
-    static constexpr int _uper_ ## Name ## OptionalIndex = decltype(_uper_optional_counter(detail::num<>()))::value; \
-    static constexpr auto _uper_optional_counter(detail::num<decltype(_uper_optional_counter(detail::num<>()))::value + 1> n) \
+    static constexpr int _uper_ ## Name ## OptionalIndex = decltype(_uper_optional_counter(uper_detail::num<>()))::value; \
+    static constexpr auto _uper_optional_counter(uper_detail::num<decltype(_uper_optional_counter(uper_detail::num<>()))::value + 1> n) \
         -> decltype(n) { return {}; } \
 public: \
     inline bool Name ## IsSet() const { return m_optionals[m_optionals.size() - _uper_ ## Name ## OptionalIndex - 1]; }
@@ -61,8 +61,8 @@ public: \
     Type Name = DefaultValue; \
     Q_PROPERTY(Type Name MEMBER Name CONSTANT) \
 private: \
-    static constexpr int _uper_ ## Name ## OptionalIndex = decltype(_uper_optional_counter(detail::num<>()))::value; \
-    static constexpr auto _uper_optional_counter(detail::num<decltype(_uper_optional_counter(detail::num<>()))::value + 1> n) \
+    static constexpr int _uper_ ## Name ## OptionalIndex = decltype(_uper_optional_counter(uper_detail::num<>()))::value; \
+    static constexpr auto _uper_optional_counter(uper_detail::num<decltype(_uper_optional_counter(uper_detail::num<>()))::value + 1> n) \
         -> decltype(n) { return {}; } \
     inline bool Name ## IsSet() const { return m_optionals[m_optionals.size() - _uper_ ## Name ## OptionalIndex - 1]; }
 }
@@ -71,7 +71,7 @@ private: \
 public: \
     void decode(UPERDecoder &decoder); \
 private: \
-    static constexpr auto _uper_OptionalCount = decltype(_uper_optional_counter(detail::num<>()))::value; \
+    static constexpr auto _uper_OptionalCount = decltype(_uper_optional_counter(uper_detail::num<>()))::value; \
     std::bitset<_uper_OptionalCount> m_optionals; \
     inline void decodeSequence(UPERDecoder &decoder) { \
         if constexpr (_uper_ExtensionMarker) { \
