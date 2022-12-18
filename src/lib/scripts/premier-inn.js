0494eecc59d0f6591e6be7f966fbae51fac7e577
Robin Grindrod <robingrindrod@live.ie>
2022-12-18 21:54:26 +0000

    Add basic script for Premier Inn hotel reservation

---
+++ src/lib/scripts/premier-inn.js
@@ -0,0 +1,20 @@
+
+function main(html) {
+    var res = JsonLd.newLodgingReservation();
+
+    res.reservationNumber = html.eval('//div[@class="refnum"]/p')[0].recursiveContent;
+
+    var times = html.eval('/html/body/table/tr/td/table/tr/td/div/div/div/div/table/tr/td/table/tr/td/div/p/b');
+    res.checkinTime = JsonLd.toDateTime(times[0].recursiveContent.replace(/\s+/g, ' '), "'From' ha - ddd dd MMM yyyy", 'en');
+    res.checkoutTime = JsonLd.toDateTime(times[1].recursiveContent.replace(/\s+/g, ' '), "'Before' ha - ddd dd MMM yyyy", 'en');
+
+    var hotelName = html.eval('/html/body/table/tr/td/table/tr/td/div/div/div/div/table/tr/td/table/tr/td/div/h2')[0].recursiveContent;
+    res.reservationFor.name = 'Premier Inn ' + hotelName;
+
+    // Not sure how to split this up generically
+    // res.reservationFor.address = html.eval('/html/body/table/tr/td/table/tr/td/div/div/div/div/table/tr/td/table/tr/td/div/table/tbody/tr/td')[1].recursiveContent;
+
+    res.underName.name = html.eval('/html/body/table/tr/td/table/tr/td/div/div/div/div/div/table/tr/td/table/tr/td/div/p')[0].recursiveContent.split('\n')[0];
+    
+    return res;
+}
