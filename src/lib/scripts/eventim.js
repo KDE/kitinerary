33997a97edf3cd758212f7f3c7d41f6f3f6958a0
Volker Krause <vkrause@kde.org>
2022-12-30 13:37:44 +0100

    Add extractor script for Eventim "TicketDirect" PDFs

--- src/lib/scripts/eventim.js
+++ src/lib/scripts/eventim.js
@@ -18,3 +18,31 @@ function parsePkPass(pass, node) {
     res.reservedTicket.ticketedSeat.seatNumber = pass.field['KEY_BACK_SEATLINE'].value;
     return res;
 }
+
+function parseTicketDirect(pdf, node, triggerNode)
+{
+    const text = pdf.pages[0].textInRect(0.0, 0.0, 0.875, 0.5);
+
+    let res = JsonLd.newEventReservation();
+    res.reservationNumber = text.match(/Ticket Nummer: (\d+-\d+)\n/)[1];
+    const dt = text.match(/([A-Z][a-z]+, \d{2}\.\d{2}\.\d{4}).*\n.*(\d{2}:\d{2}) Uhr(?:.*\n.*?(\d{2}:\d{2}) Uhr.*)?/);
+
+    if (dt[3]) {
+        res.reservationFor.doorTime = JsonLd.toDateTime(dt[1] + dt[2], 'dddd, dd.MM.yyyyhh:mm', 'de');
+        res.reservationFor.startDate = JsonLd.toDateTime(dt[1] + dt[3], 'dddd, dd.MM.yyyyhh:mm', 'de');
+    } else {
+        res.reservationFor.startDate = JsonLd.toDateTime(dt[1] + dt[2], 'dddd, dd.MM.yyyyhh:mm', 'de');
+    }
+
+    res.reservationFor.name = text.substr(dt.index + dt[0].length).match(/(.+\n.*)\n/)[1];
+    res.reservedTicket.ticketToken = 'qrcode:' + text.match(/\b(\d{22})\b/)[1];
+
+    const seat = text.match(/Block: (.*) +Reihe: (.*) +Sitz: (.*)/);
+    if (seat) {
+        res.reservedTicket.ticketedSeat.seatSection = seat[1];
+        res.reservedTicket.ticketedSeat.seatRow = seat[2];
+        res.reservedTicket.ticketedSeat.seatNumber = seat[3];
+    }
+
+    return res;
+}
