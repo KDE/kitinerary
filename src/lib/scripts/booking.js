da874e840abe7db3c2aa688aff8730d5bb2ceaf0
Volker Krause <vkrause@kde.org>
2023-01-26 18:22:20 +0100

    Find booking.com modification URLs independent of the document language

--- src/lib/scripts/booking.js
+++ src/lib/scripts/booking.js
@@ -103,7 +103,7 @@ function parseHtmlCommon(doc, node, res)
             res.reservationNumber = href.match(/mailto:(\d+)-/)[1];
         } else if (aElem.attribute('universal') == 'true') {
             res.reservationFor.name = aElem.content;
-        } else if (aElem.content.match(/modify/) && href.startsWith("https:")) {
+        } else if (!res.modifyReservationUrl && href.startsWith("https:") && (href.match(/pbsource=email_change;/) || href.match(/pbsource=conf_email_modify;/))) {
             res.modifyReservationUrl = href;
         }
     }
