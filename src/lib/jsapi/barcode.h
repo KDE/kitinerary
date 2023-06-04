ac4fb1795d1c80a03af9e6170951ccc1e9487b45
Volker Krause <vkrause@kde.org>
2023-06-04 11:03:57 +0200

    Remove the explicit ERA SSB decoding JS API
    This is no longer needed thanks to the more generic nested extraction API.

--- src/lib/jsapi/barcode.h
+++ src/lib/jsapi/barcode.h
@@ -51,14 +51,6 @@ public:
      */
     Q_INVOKABLE QString decodeAnyBarcode(const QVariant &img) const;
 
-    /** Decode an ERA SSB ticket barcode.
-     *  @param s A QByteArray containing the raw ERA SSB barcode data.
-     *  @param versionOverride Override version auto-detection. Useful for tickets that are known to
-     *  fill their version field incorrectly.
-     *  @returns An instance of SSBTicket.
-     */
-    Q_INVOKABLE QVariant decodeEraSsbTicket(const QByteArray &s, int versionOverride = 0) const;
-
     ///@cond internal
     void setDecoder(const BarcodeDecoder *decoder);
     ///@endcond
