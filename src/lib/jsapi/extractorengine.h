3f61850b5c3ecfdf0a84b27e612e66098aced0da
Volker Krause <vkrause@kde.org>
2023-06-04 11:10:32 +0200

    Document the new JS API

--- src/lib/jsapi/extractorengine.h
+++ src/lib/jsapi/extractorengine.h
@@ -17,14 +17,36 @@ class ExtractorEngine : public QObject
 {
     Q_OBJECT
 public:
+    ///@cond internal
     explicit ExtractorEngine(QObject *parent = nullptr);
     ~ExtractorEngine();
 
     void setEngine(KItinerary::ExtractorEngine *engine);
     void setCurrentNode(const ExtractorDocumentNode &node);
     void clear();
+    ///@endcond
 
+    /** Run the extractor on @p data.
+     *  Use this if the data to extract needs to be decoded by an extractor script first
+     *  and is available as raw byte array.
+     *  You do not need to call this for document parts that the extractor recognizes itself.
+     *
+     *  A new document node for @p data is added below the node currently processed,
+     *  and that node is returned.
+     *
+     *  @see KItinerary::ExtractorEngine
+     */
     Q_INVOKABLE KItinerary::ExtractorDocumentNode extract(const QByteArray &data);
+    /** Run the extractor on @p content of type @p mimeType.
+     *  Use this if the data to extract needs to be decoded by an extractor script first
+     *  and is available already in decoded form in a suitable data type.
+     *  You do not need to call this for document parts that the extractor recognizes itself.
+     *
+     *  A new document node for @p data is added below the node currently processed,
+     *  and that node is returned.
+     *
+     *  @see KItinerary::ExtractorEngine
+     */
     Q_INVOKABLE KItinerary::ExtractorDocumentNode extract(const QVariant &content, const QString &mimeType);
 
 private:
