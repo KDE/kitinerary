68a376c01731e8083d31f5675e19deee6919bc04
Volker Krause <vkrause@kde.org>
2023-06-17 16:55:55 +0200

    Simplify Trenitalia extractor
    We don't need to do manual page iteration here anymore, triggering on
    the barcodes does that automatically nowadays.

--- src/lib/scripts/trenitalia.js
+++ src/lib/scripts/trenitalia.js
@@ -45,67 +45,61 @@ function parseSsb(ssb, node) {
     return res;
 }
 
-function parsePdf(pdf, node) {
-    var reservations = new Array();
+function parsePdf(pdf, node, triggerNode) {
+    let reservations = [];
+    const page = pdf.pages[triggerNode.location];
+    const text = page.text;
 
-    for (var i = 0; i < pdf.pageCount; ++i) {
-        var page = pdf.pages[i];
-        var text = page.text;
+    var res = JsonLd.newTrainReservation();
+    var pnr = text.match(/PNR: (\S+)/);
+    if (pnr) {
+        res.reservationNumber = pnr[1];
+    }
 
-        var res = JsonLd.newTrainReservation();
-        var pnr = text.match(/PNR: (\S+)/);
-        if (pnr) {
-            res.reservationNumber = pnr[1];
-        }
+    const leftHeaderText = page.textInRect(0.0, 0.15, 0.33, 0.25);
+    const midHeaderText = page.textInRect(0.33, 0.15, 0.65, 0.25);
+    const rightHeaderText = page.textInRect(0.65, 0.15, 1.0, 0.25);
 
-        const leftHeaderText = page.textInRect(0.0, 0.15, 0.33, 0.25);
-        const midHeaderText = page.textInRect(0.33, 0.15, 0.65, 0.25);
-        const rightHeaderText = page.textInRect(0.65, 0.15, 1.0, 0.25);
+    const train = rightHeaderText.match(/(?:Train|Treno|Zug)(?:\/Train)?:[ \n](.*)\n/);
+    res.reservationFor.trainNumber = train[1];
 
-        const train = rightHeaderText.match(/(?:Train|Treno|Zug)(?:\/Train)?:[ \n](.*)\n/);
-        if (!train) {
+    const departure_time = leftHeaderText.match(/(\d{2}:\d{2}) - (\d{2}\/\d{2}\/\d{4})/)
+    const arrival_time = midHeaderText.match(/(\d{2}:\d{2}) - (\d{2}\/\d{2}\/\d{4})/)
+    res.reservationFor.departureTime = JsonLd.toDateTime(departure_time[2] + departure_time[1], "dd/MM/yyyyhh:mm", "it");
+    res.reservationFor.arrivalTime = JsonLd.toDateTime(arrival_time[2] + arrival_time[1], "dd/MM/yyyyhh:mm", "it");
+
+    const dep = leftHeaderText.match(/(?:Stazione di Partenza|Departure station|Abfahrtsbahnhof|Gare de départ)(?:\/From)?\n+(.*)\n/);
+    res.reservationFor.departureStation.name = dep[1];
+    const arr = midHeaderText.match(/(?:Stazione di Arrivo|Arrival station|Ankunft Bahnhof|Gare d'arrivée)(?:\/To)?\n+(.*)\n/);
+    res.reservationFor.arrivalStation.name = arr[1];
+
+    const barcodes = node.findChildNodes({ scope: "Descendants", mimeType: "internal/era-ssb", field: "issuerCode", match: "83" }).concat(node.findChildNodes({ scope: "Descendants", mimeType: "internal/uic9183", field: "carrierId", match: "83" }));
+    var offset = 0;
+    const passengerColumn = page.textInRect(0.0, 0.3, 0.27, 1.0);
+    for (let j = 0; j < barcodes.length; ++j) {
+        if (barcodes[j].location != triggerNode.location) {
             continue;
         }
-        res.reservationFor.trainNumber = train[1];
-
-        const departure_time = leftHeaderText.match(/(\d{2}:\d{2}) - (\d{2}\/\d{2}\/\d{4})/)
-        const arrival_time = midHeaderText.match(/(\d{2}:\d{2}) - (\d{2}\/\d{2}\/\d{4})/)
-        res.reservationFor.departureTime = JsonLd.toDateTime(departure_time[2] + departure_time[1], "dd/MM/yyyyhh:mm", "it");
-        res.reservationFor.arrivalTime = JsonLd.toDateTime(arrival_time[2] + arrival_time[1], "dd/MM/yyyyhh:mm", "it");
-
-        const dep = leftHeaderText.match(/(?:Stazione di Partenza|Departure station|Abfahrtsbahnhof|Gare de départ)(?:\/From)?\n+(.*)\n/);
-        res.reservationFor.departureStation.name = dep[1];
-        const arr = midHeaderText.match(/(?:Stazione di Arrivo|Arrival station|Ankunft Bahnhof|Gare d'arrivée)(?:\/To)?\n+(.*)\n/);
-        res.reservationFor.arrivalStation.name = arr[1];
-
-        const barcodes = node.findChildNodes({ scope: "Descendants", mimeType: "internal/era-ssb", field: "issuerCode", match: "83" }).concat(node.findChildNodes({ scope: "Descendants", mimeType: "internal/uic9183", field: "carrierId", match: "83" }));
-        var offset = 0;
-        const passengerColumn = page.textInRect(0.0, 0.3, 0.27, 1.0);
-        for (let j = 0; j < barcodes.length; ++j) {
-            if (barcodes[j].location != i) {
-                continue;
-            }
-            let personalRes = JsonLd.clone(res);
-            var name = passengerColumn.substr(offset).match(/(?:Passenger Name|Nome Passeggero|Nom du Passager)(?:\/Passenger\n *name)?.*\n(?:    .*\n)* *((?:\w+|\-\-).*?)(?:  |\n)/);
-            offset += name.index + name[0].length;
-            if (name[1] !== "--") {
-                personalRes.underName.name = name[1];
-            } else {
-                personalRes.underName.name = "Passenger " + (j + 1);
-            }
-
-            var coach = text.match(/(?:Coaches|Carrozza|Wagen|Voiture)(?:\/Coach)?: +(\S+)/);
-            if (coach) {
-                personalRes.reservedTicket.ticketedSeat.seatSection = coach[1];
-            }
-
-            if (barcodes[j].result[0]['@type'] == 'TrainReservation') {
-                personalRes = JsonLd.apply(barcodes[j].result[0], personalRes);
-            } else {
-                personalRes.reservedTicket = JsonLd.apply(barcodes[j].result[0], personalRes.reservedTicket);
-            }
-            reservations.push(personalRes);
+        let personalRes = JsonLd.clone(res);
+        var name = passengerColumn.substr(offset).match(/(?:Passenger Name|Nome Passeggero|Nom du Passager)(?:\/Passenger\n *name)?.*\n(?:    .*\n)* *((?:\w+|\-\-).*?)(?:  |\n)/);
+        offset += name.index + name[0].length;
+        if (name[1] !== "--") {
+            personalRes.underName.name = name[1];
+        } else {
+            personalRes.underName.name = "Passenger " + (j + 1);
         }
+
+        var coach = text.match(/(?:Coaches|Carrozza|Wagen|Voiture)(?:\/Coach)?: +(\S+)/);
+        if (coach) {
+            personalRes.reservedTicket.ticketedSeat.seatSection = coach[1];
+        }
+
+        if (barcodes[j].result[0]['@type'] == 'TrainReservation') {
+            personalRes = JsonLd.apply(barcodes[j].result[0], personalRes);
+        } else {
+            personalRes.reservedTicket = JsonLd.apply(barcodes[j].result[0], personalRes.reservedTicket);
+        }
+        reservations.push(personalRes);
     }
 
     return reservations;
