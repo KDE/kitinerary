14fb6f7fc46b76d0a9d26bc7e090a1b21b346e3a
Volker Krause <vkrause@kde.org>
2023-08-12 12:43:51 +0200

    Make PriceUtil directly usable from QML

--- src/lib/priceutil.h
+++ src/lib/priceutil.h
@@ -8,6 +8,8 @@
 
 #include "kitinerary_export.h"
 
+#include <qobjectdefs.h>
+
 class QString;
 class QStringView;
 class QVariant;
@@ -15,28 +17,29 @@ class QVariant;
 namespace KItinerary {
 
 /** Utilities for dealing with price/currency information of items. */
-class PriceUtil
+class KITINERARY_EXPORT PriceUtil
 {
+    Q_GADGET
 public:
     /** Returns @c true if @p item has valid price and currency information. */
-    KITINERARY_EXPORT static bool hasPrice(const QVariant &item);
+    Q_INVOKABLE static bool hasPrice(const QVariant &item);
 
     /** Returns @c true if @p item can have price/currency information. */
-    KITINERARY_EXPORT static bool canHavePrice(const QVariant &item);
+    Q_INVOKABLE static bool canHavePrice(const QVariant &item);
 
     /** Returns the price value from @p item. */
-    KITINERARY_EXPORT static double price(const QVariant &item);
+    Q_INVOKABLE static double price(const QVariant &item);
     /** Returns the currency value from @p item. */
-    KITINERARY_EXPORT static QString currency(const QVariant &item);
+    Q_INVOKABLE static QString currency(const QVariant &item);
 
     /** Sets @p price and @p currency on @p item. */
-    KITINERARY_EXPORT static void setPrice(QVariant &item, double price, const QString &currency);
+    Q_INVOKABLE static void setPrice(QVariant &item, double price, const QString &currency);
 
     /** Returns the number of decimals to represent the sub-unit of @p currency.
      *  @param currency ISO 4217 currency code
      */
     // TODO this is rather material for KI18nLocaleData
-    KITINERARY_EXPORT static int decimalCount(QStringView currency);
+    Q_INVOKABLE static int decimalCount(QStringView currency);
 
     // TODO add method for computing the total price of a set of items
 };
