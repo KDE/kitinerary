d1a52e530794b21fb8503c9968809c1db59d91b5
Volker Krause <vkrause@kde.org>
2023-10-23 18:41:36 +0200

    Handle multi-line airport names in Qatar Airways tickets

--- src/lib/scripts/qatar-airways.js
+++ src/lib/scripts/qatar-airways.js
@@ -3,25 +3,39 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
+function parseAirport(input) {
+    let l = [];
+    for (line of input.split('\n')) {
+        l.push(line.replace(/^(?:.*?)?  +/, ''));
+    }
+    return l.join(' ').match(/(\S{2,}.*\S) \(([A-Z]{3})\), (.*)/);
+}
+
 function parseETicket(pdf) {
     const pnr = pdf.pages[0].text.match(/Booking Reference +([A-Z0-9]{6})\n/)[1];
     const leftCol = pdf.pages[0].textInRect(0.0, 0.0, 0.44, 1.0);
     let reservations = [];
     for(let idx = 0;;) {
-        const leg = leftCol.substr(idx).match(/([A-Z0-9]{2})(\d{1,4})  +... \(([A-Z]{3})\), (.*)\n(.*?)  +\S*, (.*)\n.*\n *... \(([A-Z]{3})\), (.*)\n.*, (.*)\n/);
-        if (!leg)
+        // select flight number until second date/time
+        const row = leftCol.substr(idx).match(/[A-Z0-9]{2} *\d{1,4}  [\s\S]*?\S+, \d+ \S+ \d{4} \d\d:\d\d\n[\s\S]*?\S+, \d+ \S+ \d{4} \d\d:\d\d(?: \+.*)?\n/);
+        if (!row)
             break;
-        idx += leg.index + leg[0].length;
+        const flight = row[0].match(/([A-Z0-9]{2}) *(\d{1,4}).*\n(.*\S)  +/);
+        const depTime = row[0].match(/\S+, (\d+ \S+ \d{4} \d\d:\d\d)\n/);
+        const arrTime = row[0].substr(depTime.index + depTime[0].length).match(/\S+, (\d+ \S+ \d{4} \d\d:\d\d)(?: \+.*)?\n/);
+        const departure = parseAirport(row[0].substr(0, depTime.index));
+        const arrival = parseAirport(row[0].substr(depTime.index + depTime[0].length, arrTime.index));
+        idx += row.index + row[0].length;
         let res = JsonLd.newFlightReservation();
-        res.reservationFor.airline.iataCode = leg[1];
-        res.reservationFor.flightNumber = leg[2];
-        res.reservationFor.departureAirport.iataCode = leg[3];
-        res.reservationFor.departureAirport.name = leg[4];
-        res.reservationFor.airline.name = leg[5];
-        res.reservationFor.departureTime = JsonLd.toDateTime(leg[6], 'd MMM yyyy hh:mm', 'en');
-        res.reservationFor.arrivalAirport.iataCode = leg[7];
-        res.reservationFor.arrivalAirport.name = leg[8];
-        res.reservationFor.arrivalTime = JsonLd.toDateTime(leg[9], 'd MMM yyyy hh:mm', 'en');
+        res.reservationFor.airline.iataCode = flight[1];
+        res.reservationFor.flightNumber = flight[2];
+        res.reservationFor.departureAirport.iataCode = departure[2];
+        res.reservationFor.departureAirport.name = departure[3];
+        res.reservationFor.airline.name = flight[3];
+        res.reservationFor.departureTime = JsonLd.toDateTime(depTime[1], 'd MMM yyyy hh:mm', 'en');
+        res.reservationFor.arrivalAirport.iataCode = arrival[2];
+        res.reservationFor.arrivalAirport.name = arrival[3];
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(arrTime[1], 'd MMM yyyy hh:mm', 'en');
         res.reservationNumber = pnr;
         reservations.push(res);
     }
