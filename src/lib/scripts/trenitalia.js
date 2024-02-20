2d5d5a488b964ace162735fdfaa2bc7df8f83a3c
Roberto Guido <info@madbob.org>
2024-02-20 17:53:59 +0100

    Fixed and improved parser for Trenitalia

--- src/lib/scripts/trenitalia.js
+++ src/lib/scripts/trenitalia.js
@@ -47,7 +47,7 @@ function parseSsb(ssb, node) {
 
 function parsePdf(pdf, node, triggerNode) {
     let reservations = [];
-    const page = pdf.pages[triggerNode.location];
+    const page = pdf.pages[0];
     const text = page.text;
 
     var res = JsonLd.newTrainReservation();
@@ -73,14 +73,12 @@ function parsePdf(pdf, node, triggerNode) {
     const arr = midHeaderText.match(/(?:Stazione di Arrivo|Arrival station|Ankunft Bahnhof|Gare d'arrivée)(?:\/To)?\n+(.*)\n/);
     res.reservationFor.arrivalStation.name = arr[1];
 
-    const barcodes = node.findChildNodes({ scope: "Descendants", mimeType: "internal/era-ssb", field: "issuerCode", match: "83" }).concat(node.findChildNodes({ scope: "Descendants", mimeType: "internal/uic9183", field: "carrierId", match: "83" }));
-    var offset = 0;
     const passengerColumn = page.textInRect(0.0, 0.3, 0.27, 1.0);
+    const passengers = passengerColumn.match(/(?:Passenger Name|Nome Passeggero|Nom du Passager)/);
+    var offset = 0;
     let seatOffset = 0;
-    for (let j = 0; j < barcodes.length; ++j) {
-        if (barcodes[j].location != triggerNode.location) {
-            continue;
-        }
+
+    for (let j = 0; j < passengers.length; j++) {
         let personalRes = JsonLd.clone(res);
         var name = passengerColumn.substr(offset).match(/(?:Passenger Name|Nome Passeggero|Nom du Passager)(?:\/Passenger\n *name)?.*\n(?:    .*\n)* *((?:\w+|\-\-).*?)(?:  |\n)/);
         offset += name.index + name[0].length;
@@ -93,12 +91,6 @@ function parsePdf(pdf, node, triggerNode) {
         var coach = text.match(/(?:Coaches|Carrozza|Wagen|Voiture)(?:\/Coach)?: +(\S+)/);
         if (coach) {
             personalRes.reservedTicket.ticketedSeat.seatSection = coach[1];
-        }
-
-        if (barcodes[j].result[0]['@type'] == 'TrainReservation') {
-            personalRes = JsonLd.apply(barcodes[j].result[0], personalRes);
-        } else {
-            personalRes.reservedTicket = JsonLd.apply(barcodes[j].result[0], personalRes.reservedTicket);
         }
 
         // fallback seat parsing for unparsable ERA FCB tickets
@@ -124,12 +116,79 @@ function parseEvent(event)
     res.reservationFor.arrivalTime = JsonLd.readQDateTime(event, 'dtEnd');
     res.reservationFor.departureStation.name = event.location;
     res.reservationFor.arrivalStation.name = event.description.match(event.location + "-(.*?);")[1];
-    res.reservationFor.trainNumber = event.description.match(/Train: (.*?),/)[1];
-    res.reservationNumber = event.description.match(/pnr code ([A-Z0-9]{6})\b/)[1];
-    const seat = event.description.match(/Coach (.*?), Position (.*?)[;\b]/);
+    res.reservationFor.trainNumber = event.description.match(/(?:Train|Treno|Zug): (.*?),/)[1];
+
+    const code = event.description.match(/(?:pnr code|codice pnr) ([A-Z0-9]{6})\b/);
+    if (code) {
+        res.reservationNumber = code[1];
+    }
+
+    const seat = event.description.match(/(?:Coach|Carrozza||Wagen|Voiture) (.*?), (?:Position|Posti) (.*?)[;\b]/);
     if (seat) {
         res.reservedTicket.ticketedSeat.seatSection = seat[1];
         res.reservedTicket.ticketedSeat.seatNumber = seat[2];
     }
+
     return res;
+}
+
+function parseHtml(html)
+{
+    let reservations = [];
+
+    // Informations are organized in a different HTML hierarchy when rappresenting
+    // - a single trip for a single person
+    // - many trips for a single person
+    // - many trip for many people
+    // Here we try to move the text offset between the different blocks, using the
+    // total number of trains mentioned within the whole document as a reference
+    const blocks = html.eval("//b[text()='Treno:']");
+    const totalTrains = blocks.length;
+    let fullText = blocks[0].parent.parent.parent.recursiveContent;
+    let offset = 0;
+
+    // All trips involved in a booking are managed together, with a single
+    // "check-in" link and a single panel to modify. For convenience, those
+    // links are attached to each trip
+    const checkinUrl = html.eval('//a[contains(@href, "/self-check-in")]')[0].attribute('href');
+    const modifyReservationUrl = html.eval('//a[contains(@href, "/user-area/purchase/ticketDetail")]')[0].attribute('href');
+
+    for(let j = 0; j < totalTrains; j++) {
+        let currentText = fullText.substr(offset);
+        let train = currentText.match(/(?:Train|Treno|Zug): (.*?) *del *([0-9\/]*)/);
+
+        if (train == null) {
+            offset = 0;
+            fullText = blocks[j].parent.parent.parent.recursiveContent;
+            currentText = fullText;
+            train = currentText.match(/(?:Train|Treno|Zug): (.*?) *del *([0-9\/]*)/);
+        }
+
+        const date = train[2].trim();
+        const start = currentText.match(/(?:Partenza) ?: (.*?)\(Ore: ([0-9:]*)\)/);
+        const end = currentText.match(/(?:Arrivo) ?: (.*?)\(Ore: ([0-9:]*)\)/);
+        const name = currentText.match(/(?:Nome Viaggiatore) ?: ?(.*?);/);
+        offset += end.index + end[1].length + end[2].length;
+
+        let res = JsonLd.newTrainReservation();
+        res.reservationFor.trainNumber = train[1];
+        res.reservationFor.departureStation.name = start[1];
+        res.reservationFor.arrivalStation.name = end[1];
+        res.reservationFor.departureTime = JsonLd.toDateTime(date + start[2], "dd/MM/yyyyhh:mm", "it");
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(date + end[2], "dd/MM/yyyyhh:mm", "it");
+
+        // Tickets for trains not directly operated by Trenitalia (e.g. Trenord) are
+        // often not assigned to a specific name, so this value is not always explicit
+        if (name) {
+            res.underName.name = name[1];
+        }
+
+        res.checkinUrl = checkinUrl;
+        res.modifyReservationUrl = modifyReservationUrl;
+        ExtractorEngine.extractPrice(currentText, res);
+
+        reservations.push(res);
+    }
+
+    return reservations;
 }
