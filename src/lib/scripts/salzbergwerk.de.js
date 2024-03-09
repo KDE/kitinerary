62c547460be396b185ff24081ff0de8d49581449
David Pilarčík <meow@charliecat.space>
2024-03-09 14:21:06 +0100

    improve `salzbergwekr.de` extractor: address extraction from text

--- src/lib/scripts/salzbergwerk.de.js
+++ src/lib/scripts/salzbergwerk.de.js
@@ -9,15 +9,15 @@ function main(pdf, node, barcode) {
     let reservationNumber = text.match(/[0-9]{12}/gm)[0]
     let tourStart         = text.match(/([0-9]+(\.[0-9]+)+) at (0?[0-9]|1[0-9]|2[0-3]):[0-9]{2}/gm)[0]
     let tourName          = text.match(/This is your ticket for (.*?) on the/)[1]
-    /*let arrivalAddress    = text.match(/ARRIVAL\S+ROUTE PLANNER\S+(.*)\S+More detailed arrival\S+(.*)\S+information you will find under\S+D - ([0-9]+)\S(.*)\S+www\.salzbergwerk\.de/gm)*/
+    let arrivalAddress    = /ARRIVAL\s+ROUTE PLANNER\n(.*?)\s+More detailed arrival\n(.*)\s+information you will find under\n[A-Z] - ([0-9]+) (.*?)\s+www\.salzbergwerk\.de/gm.exec(text)
 
     res.reservationFor.startDate = JsonLd.toDateTime(tourStart.replace("at ", ""), "dd.MM.yyyy hh:mm", "de")
     res.reservationFor.name = tourName
 
-    res.reservationFor.location.name = "Parking Salzbergwerk";
-    res.reservationFor.location.address.streetAddress = "Salzburger Straße 24";
-    res.reservationFor.location.address.postalCode = "83471";
-    res.reservationFor.location.address.addressLocality = "Berchtesgaden";
+    res.reservationFor.location.name = arrivalAddress[1];
+    res.reservationFor.location.address.streetAddress =  arrivalAddress[2];
+    res.reservationFor.location.address.postalCode =  arrivalAddress[3];
+    res.reservationFor.location.address.addressLocality =  arrivalAddress[4];
     res.reservationFor.location.address.addressCountry = "DE";
 
     res.reservedTicket.ticketToken = "datamatrix:" + reservationNumber
