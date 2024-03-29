d53a8b6dcf3006f8287c23fc736849c0733c74ed
David Pilarčík <meow@charliecat.space>
2024-03-29 10:35:18 +0100

    add: Ticketportal event ticket extractor
    This commit adds an extractor for Ticketportal (https://ticketportal.cz) so called hometicket .pdf ticket.
    
    This extractor only supports the czech localization as that was the only one i had to test with. Ticketportal operates also in Slovakia, Hungary and Bulgaria.
    
    Problem with this extractor and ticketportal itself is  that that they dont write the name of the event in text but as part of an .png. Another problem is that ticketportal allows for unstandardised venue location strings.

---
+++ src/lib/scripts/ticketportal.js
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2024 David Pilarčík <meow@charliecat.space>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+function hometicket(content) {
+    
+    const text = content.text.split("NEZBYTNÉ OHRANIČENÍ VSTUPENKY (MUSÍ BÝT I POD VSTUPENKOU !)").slice(1)
+    const reservations = []
+
+    let codeRegex = /\s+KOPIE [0-9A-Z-]{36}\n\s+([0-9]{6})\s+([0-9]{10})\n\s+eTicket číslo:/ // [Reservation Number, Ticket Token]
+    let priceRegex = /eTicket číslo:\n\s+([0-9]+.[0-9]+.[0-9]+ od [0-9]+:[0-1]+ hod.)\n\s+(.*?)\n\s+Cena: ([0-9,]+ Kč|€)/ // [Start Date, Location Name, Price]
+    let organizatorRegex = /Pořadatel: (.*?); IČ: [0-9]+; DIČ: [A-Z0-9]+; Cena .*\n/ // [Organizer]
+
+    for (let i = 0; i < text.length; i++) {
+        let res = JsonLd.newEventReservation();
+        let page = text[i]
+
+        let code = codeRegex.exec(page)
+        let datePlacePrice = priceRegex.exec(page)
+        //let organizer = organizatorRegex.exec(page)
+
+        res.reservationFor.location.name = datePlacePrice[2]
+        res.reservationFor.startDate = JsonLd.toDateTime(datePlacePrice[1], "dd.MM.yyyy od hh:mm hod.", "cz") // 11.10.2022 od 19:00 hod.
+
+        res.priceCurrency = datePlacePrice[3].includes("Kč") ? "CZK" : "EUR"
+        res.totalPrice = Number(datePlacePrice[3].replace(/Kč|Eur|€/, "").replace(",", ".").replace("-", 0))
+
+        res.reservedTicket.ticketToken = "qrCode:" + code[2]
+        res.reservationNumber = code[1]
+
+        reservations.push(res)
+    }
+    
+
+    return reservations
+}
