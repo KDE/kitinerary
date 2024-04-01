a26dc16b7fab8389be3bfb5dc2633708923b5fbf
David Pilarčík <meow@charliecat.space>
2024-04-01 17:08:48 +0200

    add(ticketportal): add ticketportal pkpass extractor

--- src/lib/scripts/ticketportal.js
+++ src/lib/scripts/ticketportal.js
@@ -14,7 +14,7 @@ function hometicket(content) {
 
         let code = codeRegex.exec(page.text)
         let datePlacePrice = priceRegex.exec(page.text)
-        //let organizer = organizatorRegex.exec(page)
+        let organizer = organizatorRegex.exec(page)
 
         res.reservationFor.location.name = datePlacePrice[2]
         res.reservationFor.startDate = JsonLd.toDateTime(datePlacePrice[1], "dd.MM.yyyy od hh:mm hod.", "cz") // 11.10.2022 od 19:00 hod.
@@ -25,9 +25,41 @@ function hometicket(content) {
         res.reservedTicket.ticketToken = "qrCode:" + code[2]
         res.reservationNumber = code[1]
 
+        res.provider = { '@type': 'Organization', name: organizer[1] };
+
         reservations.push(res)
     }
     
 
     return reservations
+}
+
+function pkpassTicket(pass, node) {
+
+    const res = node.result[0];
+
+    console.log(JSON.stringify(pass.field, null, 4))
+
+    res.reservationNumber = pass.field["af_2"].value
+
+    res.reservationFor.name = pass.field["pf_0"].value
+    res.reservationFor.startDate = JsonLd.toDateTime(pass.field["sf_0"].value, "d. MMMM yyyy hh:mm", "sk") // 7. augusta 2022 19:00
+
+    res.reservationFor.location = JsonLd.newObject("Place");
+    res.reservationFor.location.name = pass.field["af_0"].value;
+
+    res.reservedTicket.name = pass.field["hf_0"].value
+
+    /*
+    // In sample: Nmiest / 0 / 0 
+    // Needs verifying
+
+    let seat = pass.field["af_1"].value.split(' / ')
+
+    res.reservedTicket.ticketedSeat.seatSection = seat[0]
+    res.reservedTicket.ticketedSeat.seatRow = seat[1]
+    res.reservedTicket.ticketedSeat.seatNumber = seat[2]
+    */
+
+    return res
 }
