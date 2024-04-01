eea154cd7bfbbe0589ba47f7d8413f58ddb53cc9
David Pilarčík <meow@charliecat.space>
2024-04-01 16:26:09 +0200

    fix(ticketportal): using `content.pages` to interate over pages

--- src/lib/scripts/ticketportal.js
+++ src/lib/scripts/ticketportal.js
@@ -2,20 +2,18 @@
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 function hometicket(content) {
-    
-    const text = content.text.split("NEZBYTNÉ OHRANIČENÍ VSTUPENKY (MUSÍ BÝT I POD VSTUPENKOU !)").slice(1)
+
     const reservations = []
 
     let codeRegex = /\s+KOPIE [0-9A-Z-]{36}\n\s+([0-9]{6})\s+([0-9]{10})\n\s+eTicket číslo:/ // [Reservation Number, Ticket Token]
     let priceRegex = /eTicket číslo:\n\s+([0-9]+.[0-9]+.[0-9]+ od [0-9]+:[0-1]+ hod.)\n\s+(.*?)\n\s+Cena: ([0-9,]+ Kč|€)/ // [Start Date, Location Name, Price]
     let organizatorRegex = /Pořadatel: (.*?); IČ: [0-9]+; DIČ: [A-Z0-9]+; Cena .*\n/ // [Organizer]
 
-    for (let i = 0; i < text.length; i++) {
+    for (page of content.pages) {
         let res = JsonLd.newEventReservation();
-        let page = text[i]
 
-        let code = codeRegex.exec(page)
-        let datePlacePrice = priceRegex.exec(page)
+        let code = codeRegex.exec(page.text)
+        let datePlacePrice = priceRegex.exec(page.text)
         //let organizer = organizatorRegex.exec(page)
 
         res.reservationFor.location.name = datePlacePrice[2]
