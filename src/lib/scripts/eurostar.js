d351239954d7e1152d78d5ebb76d9f3564ac818d
Volker Krause <vkrause@kde.org>
2024-04-02 17:14:45 +0200

    Prettify data extracted from Eurostar ERA ELB barcodes
    (cherry picked from commit 02722255ca1b6fa4858097220db16b2ced785090)

--- src/lib/scripts/eurostar.js
+++ src/lib/scripts/eurostar.js
@@ -3,6 +3,10 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
+function stripLeadingZeros(s) {
+    return s.match(/^0*(.*)/)[1];
+}
+
 function parseBarcode(elb, node) {
     let res = JsonLd.newTrainReservation();
     res.reservationNumber = elb.pnr;
@@ -11,12 +15,13 @@ function parseBarcode(elb, node) {
     res.reservationFor.arrivalStation.name = elb.segment1.arrivalStation;
     res.reservationFor.arrivalStation.identifier = 'benerail:' + elb.segment1.arrivalStation;
     res.reservationFor.departureDay = elb.segment1.departureDate(node.contextDateTime);
-    res.reservationFor.trainNumber = elb.segment1.trainNumber;
+    res.reservationFor.trainName = 'Eurostar';
+    res.reservationFor.trainNumber = stripLeadingZeros(elb.segment1.trainNumber);
     res.reservationFor.provider.identifier = 'uic:' + elb.futureUse.substr(1, 4);
     res.reservedTicket.ticketNumber = elb.tcnCode;
     res.reservedTicket.ticketToken = 'pdf417:' + elb.rawData;
-    res.reservedTicket.ticketedSeat.seatSection = elb.segment1.coachNumber;
-    res.reservedTicket.ticketedSeat.seatNumber = elb.segment1.seatNumber;
+    res.reservedTicket.ticketedSeat.seatSection = stripLeadingZeros(elb.segment1.coachNumber);
+    res.reservedTicket.ticketedSeat.seatNumber = stripLeadingZeros(elb.segment1.seatNumber);
     res.reservedTicket.ticketedSeat.seatingType = elb.segment1.classOfTransport;
     return res;
 }
