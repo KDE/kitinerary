dcf3b3e8238035f8d0e8176a269d55f42006b161
Volker Krause <vkrause@kde.org>
2024-06-04 17:19:56 +0200

    Support Amadeus Cytric booking confirmations

--- src/lib/scripts/amadeus.js
+++ src/lib/scripts/amadeus.js
@@ -67,3 +67,66 @@ function parseEvent(event)
 
     return res;
 }
+
+function parseCytricEvent(ev)
+{
+    const category = ev.categories[0];
+    if (category.match(/Flight|Flug/)) {
+        let reservations = [];
+        let idx = 0;
+        while (true) {
+            let res = JsonLd.newFlightReservation();
+            const flight = ev.description.substr(idx).match(/([A-Z0-9]{2}) (.*?) (\d{1,4}) .*\n.* - (.*) \(([A-Z]{3})\)(?:, Terminal (.*?),)? .*?, (\d+ \S+ \d{4}) .*? (\d\d:\d\d)\n.*? - (.*) \(([A-Z]{3})\)(?:, Terminal (.*?),)? .*?, (\d+ \S+ \d{4}) .* (\d\d:\d\d)\n[\s\S]*?: ([A-Z0-9]{6})\n/);
+            if (!flight)
+                break;
+            idx += flight.index + flight[0].length;
+            res.reservationFor.airline.iataCode = flight[1];
+            res.reservationFor.airline.name = flight[2];
+            res.reservationFor.flightNumber = flight[3];
+
+            res.reservationFor.departureAirport.name = flight[4];
+            res.reservationFor.departureAirport.iataCode = flight[5];
+            res.reservationFor.departureTerminal = flight[6];
+            res.reservationFor.departureTime = JsonLd.toDateTime(flight[7] + flight[8], "dd MMMM yyyyhh:mm", ["en", "de"]);
+
+            res.reservationFor.arrivalAirport.name = flight[9];
+            res.reservationFor.arrivalAirport.iataCode = flight[10];
+            res.reservationFor.arrivalTerminal = flight[11];
+            res.reservationFor.arrivalTime = JsonLd.toDateTime(flight[12] + flight[13], "dd MMMM yyyyhh:mm", ["en", "de"]);
+
+            res.reservationNumber = flight[14];
+            reservations.push(res);
+        }
+        return reservations;
+    }
+    else if (category.match(/Hotel/)) {
+        let res = JsonLd.newLodgingReservation();
+        const loc = ev.location.match(/^(.*?), (.*?), (.*), (.*)$/);
+        res.reservationFor.name = loc[1];
+        res.reservationFor.address.streetAddress = loc[2];
+        res.reservationFor.address.addressLocality = loc[3];
+        res.reservationFor.address.addressCountry = loc[4];
+        res.checkinTime = ev.dtStart; // TODO this might break for date-only values?
+        res.checkoutTime = ev.dtEnd;
+        res.reservationNumber = ev.description.match(/code: (.*)\n/)[1];
+        return res;
+    }
+    else if (category.match(/Rental|Mietwagen/)) {
+        let res = JsonLd.newRentalCarReservation();
+        const rental = ev.description.match(/^(.*)\n\S+ (.*?),.*, (.*?)\(.* (\d\d:\d\d) .* (\d+ \S+ \d{4}),.*\n\S+ (.*?),.*, (.*?)\(.* (\d\d:\d\d) .*, (\d+ \S+ \d{4}).*\n.*: (.*)\n.* (\S+)\n/);
+        res.reservationFor.rentalCompany.name = rental[1];
+
+        res.pickupLocation.name = rental[2];
+        res.pickupLocation.address.streetAddress = rental[3];
+        res.pickupTime = JsonLd.toDateTime(rental[5] + rental[4], "dd MMMM yyyyhh:mm", ["en", "de"]);
+
+        res.dropoffLocation.name = rental[6];
+        res.dropoffLocation.address.streetAddress = rental[7];
+        res.dropoffTime = JsonLd.toDateTime(rental[9] + rental[8], "dd MMMM yyyyhh:mm", ["en", "de"]);
+
+        res.reservationFor.name = rental[10];
+        res.reservationNumber = rental[11];
+        return res;
+    }
+    console.log("unhandled category", category);
+}
