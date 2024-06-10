c6da9c7a2576d58acdbfe458bcada93511d8ccaa
Volker Krause <vkrause@kde.org>
2024-06-10 17:19:58 +0200

    Handle more Flixbus ticket token variants in JSON-LD data
    (cherry picked from commit 0d8b49c50f33241efdc090762ca487601f2f9022)

--- src/lib/scripts/flixbus.js
+++ src/lib/scripts/flixbus.js
@@ -17,8 +17,8 @@ function main(content, node) {
     // convert QR download links into the actual QR codes
     var res = node.result;
     for (var i = 0; i < res.length; ++i) {
-        var ticketToken = res[i].reservedTicket.ticketToken;
-        res[i].reservedTicket.ticketToken = ticketToken.replace(/^https?:\/\/api\.(?:flixbus|meinfernbus)\..{2,3}\/qrcode\/(..)\//, "qrCode:https://shop.flixbus.$1/pdfqr/");
+        res[i].reservedTicket.ticketToken = res[i].reservedTicket.ticketToken.replace(/^https?:\/\/api\.(?:flixbus|meinfernbus)\..{2,3}\/qrcode\/(..)\//, "qrCode:https://shop.flixbus.$1/pdfqr/");
+        res[i].reservedTicket.ticketToken = res[i].reservedTicket.ticketToken.replace(/^(https?:\/\/.*$/, "qrCode:$1");
 
         // their schema.org annotations also claim train trips are bus trips, fix that
         res[i] = fixTransportMode(res[i]);
