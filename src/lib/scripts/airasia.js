96561e20fdb0ddd4348c2fb7dfd3cbd514d64ad6
Volker Krause <vkrause@kde.org>
2024-06-10 17:14:48 +0200

    Handle Air Asia booking email with departure terminals

--- src/lib/scripts/airasia.js
+++ src/lib/scripts/airasia.js
@@ -9,7 +9,7 @@ function extractBookingConfirmation(html) {
     let reservations = [];
     let idx = 0;
     while (true) {
-        const flight = text.substr(idx).match(/Flight \d+\n +\S+, (.*)\n(?:.*\n)+?(\d\d: *\d\d)\n(.*) \(([A-Z]{3})\) *\n(\d\d:\d\d)\n *(\S.*) \(([A-Z]{3})\)\n(?:(.*)\n)?([A-Z0-9]{2}) +(\d{1,4})\n/);
+        const flight = text.substr(idx).match(/Flight \d+\n +\S+, (.*)\n(?:.*\n)+?(\d\d: *\d\d)\n(.*) \(([A-Z]{3})\) *\n(?:(.*)\n)?(\d\d:\d\d)\n *(\S.*) \(([A-Z]{3})\)\n(?:(.*)\n)?([A-Z0-9]{2}) +(\d{1,4})\n/);
         if (!flight)
             break;
         idx += flight.index + flight[0].length;
@@ -18,13 +18,13 @@ function extractBookingConfirmation(html) {
         res.reservationFor.departureTime = JsonLd.toDateTime(flight[1] + ' ' + flight[2], 'dd MMM yyyy hh: mm', 'en');
         res.reservationFor.departureAirport.name = flight[3];
         res.reservationFor.departureAirport.iataCode = flight[4];
-        // TODO departure terminal?
-        res.reservationFor.arrivalTime = JsonLd.toDateTime(flight[1] + ' ' + flight[5], 'dd MMM yyyy hh:mm', 'en');
-        res.reservationFor.arrivalAirport.name = flight[6];
-        res.reservationFor.arrivalAirport.iataCode = flight[7];
-        res.reservationFor.arrivalTerminal = flight[8];
-        res.reservationFor.airline.iataCode = flight[9];
-        res.reservationFor.flightNumber = flight[10];
+        res.reservationFor.departureTerminal = flight[5];
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(flight[1] + ' ' + flight[6], 'dd MMM yyyy hh:mm', 'en');
+        res.reservationFor.arrivalAirport.name = flight[7];
+        res.reservationFor.arrivalAirport.iataCode = flight[8];
+        res.reservationFor.arrivalTerminal = flight[9];
+        res.reservationFor.airline.iataCode = flight[10];
+        res.reservationFor.flightNumber = flight[11];
         res.reservationNumber = bookingNo;
         reservations.push(res);
     }
