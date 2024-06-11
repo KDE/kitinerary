9dd3a92492fdbedf1dd430c2c78fb6c489748ea4
Volker Krause <vkrause@kde.org>
2024-06-11 19:10:53 +0200

    Add tobilet.pl event ticket extractor script
    (cherry picked from commit 3f7979a9e183800c2d2f531f501ba80f8b0007d9)

---
+++ src/lib/scripts/tobilet.js
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Volker Krause <vkrause@kde.org>
+//SPDX-License-Identifier: LGPL-2.0-or-later
+
+function extractPdf(pdf, node, triggerNode) {
+    const text = pdf.pages[triggerNode.location].text;
+    console.log(text);
+
+    let res = JsonLd.newEventReservation();
+    const ev = text.match(/(.*)\n(\d{4}\.\d{2}\.\d{2}) - (\d{4}\.\d{2}\.\d{2})\n.*?: (.*)\n.*?: (.*)\n/);
+
+    res.reservationFor.name = ev[1];
+    res.reservationFor.startDate = JsonLd.toDateTime(ev[2], "yyyy.MM.dd", "pl");
+    res.reservationFor.endDate = JsonLd.toDateTime(ev[3], "yyyy.MM.dd", "pl");
+    res.reservationFor.location.address.addressLocality = ev[4];
+    res.reservationFor.location.name = ev[5];
+    res.reservedTicket.ticketToken = "qrCode:" + triggerNode.content;
+
+    return res;
+}
