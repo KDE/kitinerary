198ca01a19b8c8dcf975ccd3a8ea986d1bd69ca7
Volker Krause <vkrause@kde.org>
2024-07-11 17:17:07 +0200

    Add extractor script for Scandlines ferry tickets

---
+++ src/lib/scripts/scandlines.js
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: 2024 Volker Krause <vkrause@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+function extractPdf(pdf, node, barcode) {
+    console.log(pdf);
+    const text = pdf.text;
+    let idx = 0;
+    let reservations = [];
+    while (true) {
+        const trip = text.substr(idx).match(/:  +(\S.*\S)  +.*:  +(\d{2}.\d{2}.\d{4} \d\d:\d\d)\n.*:  +(\S.*\S)  +.*:  +(\d{2}.\d{2}.\d{4} \d\d:\d\d)\n/);
+        if (!trip)
+            break;
+        console.log(trip);
+        idx += trip.index + trip[0].length;
+
+        let res = JsonLd.newBoatReservation();
+        res.reservationFor.departureBoatTerminal.name = trip[1];
+        res.reservationFor.departureTime = JsonLd.toDateTime(trip[2], "dd.MM.yyyy hh:mm", "dk");
+        res.reservationFor.arrivalBoatTerminal.name = trip[3];
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(trip[4], "dd.MM.yyyy hh:mm", "dk");
+        res.reservedTicket.ticketToken = 'qrcode:' + barcode.content;
+        res.reservationNumber = barcode.content;
+        reservations.push(res);
+    }
+    return reservations;
+}
