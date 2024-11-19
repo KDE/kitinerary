717879c2b22ba3fc44d34346c99a90a601e07095
David Pilarčík <meow@charliecat.space>
2024-11-19 17:47:17 +0100

    add: begin droplabs extractor

--- src/lib/scripts/droplabs.js
+++ src/lib/scripts/droplabs.js
@@ -1,12 +1,18 @@
+/*
+    SPDX-FileCopyrightText: 2024 David Pilarćík <meow@charliecat.space>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+const i18n = {
+	
+}
+
 function parsePass(pass, node) {
 
 	const res = Object.assign(JsonLd.newEventReservation(), node.result[0])
 
 	res.reservationFor.startDate = JsonLd.toDateTime(pass.field['validity'].value, "dd.mm.yyyy", "sk") // 09.08.2024
 	res.reservationFor.name = pass.field["ticket"].value ?? res.reservationFor.name
-
-	//const loc = res.location = JsonLd.newObject("PostalAddress")
-	//res.location.address = pass.field['address'].value
 
 	res.underName = { '@type': 'Person', email: pass.field['email'].value }
 	res.provider = { '@type': 'Organization', name: pass.field['seller'].value };
@@ -16,10 +22,8 @@ function parsePass(pass, node) {
 	return res
 }
 
-// function parsePDF(pdf, node, barcode) {
+function parsePDF(pdf, node, barcode) {
 
-// 	console.log(pdf.text)
-// 	console.log('===================')
+	console.log(pdf, node, barcode)
 	
-// 	return node.result[0]
-// }
+}
