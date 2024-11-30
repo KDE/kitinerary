1dcad5b605d0efe98fa8ebd17fdf1bf5fea3c6ef
David Pilarčík <meow@charliecat.space>
2024-11-30 18:39:15 +0100

    add: add Leo Express PkPass extractor

--- src/lib/scripts/leoexpress.js
+++ src/lib/scripts/leoexpress.js
@@ -32,3 +32,25 @@ function extractPdf(pdf) {
     res.reservedTicket.ticketToken = 'qrcode:' + res.reservationNumber;
     return res;
 }
+
+function extractPkpass(pass, node) {
+	const res = Object.assign(JsonLd.newTrainReservation(), node.result[0]);
+
+	res.reservationFor.departureStation.name = pass.field["departure"].label;
+    res.reservationFor.departureTime = JsonLd.toDateTime(pass.field["departure"].value + " " + pass.field["departureDate"].value, "hh:mm dd.MM.yy", "cz");
+
+    res.reservationFor.arrivalStation.name = pass.field["arrival"].label;
+    res.reservationFor.departureTime = JsonLd.toDateTime(pass.field["arrival"].value + " " + (pass.field["arrivalDate"] ? pass.field["arrivalDate"].value : pass.field["departureDate"].value), "hh:mm dd.MM.yy", "cz");
+
+    res.reservationFor.trainNumber = pass.field["line"].value
+    res.reservationNumber = pass.field["code"].value
+
+	res.reservedTicket.name = pass.field["rate"].value
+	res.reservedTicket.ticketedSeat = {
+        "@type": "Seat",
+        seatNumber: pass.field["seat"].value,
+		seatingType: pass.field["className"].value,
+    }
+
+	return res
+}
