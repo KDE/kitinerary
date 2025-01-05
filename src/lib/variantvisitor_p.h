bfd8cb8b41d16ce155686fce1e835db1344cc3f2
Volker Krause <vkrause@kde.org>
2025-01-05 15:16:07 +0100

    Add VariantVisitor
    That's basically std::visit, but for a fixed set of types in a QVariant.
    We'll use that for simplifying FCB parsing code, especially once its types
    will double with the introduction of FCB v3 support.

---
+++ src/lib/variantvisitor_p.h
@@ -0,0 +1,45 @@
+/*
+    SPDX-FileCopyrightText: 2025 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef KITINERARY_VARIANTVISITOR_H
+#define KITINERARY_VARIANTVISITOR_H
+
+#include <QVariant>
+
+namespace KItinerary {
+
+/** Like std::visit, but for a fixed set of types inside a QVariant. */
+template <typename Func>
+class VariantVisitor
+{
+private:
+    Func m_func;
+
+public:
+    explicit VariantVisitor(Func func)
+        : m_func(func) {}
+
+    template <typename T, typename ...Ts>
+    auto inline visit(const QVariant &v) -> decltype(m_func(v.value<T>())) {
+        using RetT = decltype(m_func(v.value<T>()));
+        return visitImpl<RetT, T, Ts...>(v);
+    }
+
+private:
+    template <typename RetT>
+    inline RetT visitImpl(const QVariant&) { return {}; }
+    template <typename RetT, typename T, typename ...Ts>
+    inline RetT visitImpl(const QVariant &v) {
+        if (v.typeId() == qMetaTypeId<T>()) {
+            return m_func(v.value<T>());
+        }
+        return visitImpl<RetT, Ts...>(v);
+    }
+
+};
+
+}
+
+#endif
