cb4069f687bdc6b78a335c23c31becadf0493982
Volker Krause <vkrause@kde.org>
2025-05-02 16:22:11 +0200

    Use release branch for stable branch build dependencies

--- scripts/build-cmake-modules.sh
+++ scripts/build-cmake-modules.sh
@@ -10,7 +10,7 @@ LIBICAL_VERSION="v3.0.17"
 LIBXML_VERSION="v2.12.7"
 ZXING_VERSION="v2.3.0"
 KF_VERSION="v6.13.0"
-GEAR_VERSION="master"
+GEAR_VERSION="release/25.04"
 
 function build_cmake_module() {
     local repo=$1
