07d5d67a6639484b11bfde687f5ef4b768e8e309
Stephan Olbrich <stephanolbrich@gmx.de>
2025-06-18 19:17:04 +0200

    add extractor for DAV membership card
    (cherry picked from commit dd37d0b112fadf2b4b7f4b861ca057a5f8b8db79)

---
+++ src/lib/scripts/dav.js
@@ -0,0 +1,20 @@
+/*
+   SPDX-FileCopyrightText: 2025 Stephan Olbrich <stephanolbrich@gmx.de>
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+function parseMembershipCard(pdf, node) {
+    const c = pdf.text.split('\n')
+    const num = c[6].match(/\d{3}\/\d{2}\/(\d+)\*.*$/)[1]
+    let card = {
+        '@type': 'ProgramMembership',
+        programName: 'DAV Mitgliedsausweis (' + c[3].match(/^ *(\S Mitglied)$/)[1] + ')',
+        membershipNumber: num,
+        member: {
+            '@type': 'Person',
+            name: c[4],
+        },
+        token: 'barcode128:' + node.childNodes[0].childNodes[0].content,
+    };
+    return card;
+}
