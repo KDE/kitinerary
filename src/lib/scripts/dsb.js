32d2c43db402f94b1495c223a029f1f9af4bcba2
Volker Krause <vkrause@kde.org>
2025-07-07 17:28:41 +0200

    Improve DSB itinerary extraction
    - find that also on the third page, happens for complex bookings
    - drop walking sections

--- src/lib/scripts/dsb.js
+++ src/lib/scripts/dsb.js
@@ -2,22 +2,27 @@
 // SPDX-License-Identifier: LGPL-2.0-or-later
 
 function extractPdf(pdf, node, barcode) {
-    const page = pdf.pages[barcode.location + 1].text;
+    let page = pdf.pages[barcode.location + 1].text;
+    if (!page.match(/^\s*Din Rejseplan/))
+        page = pdf.pages[barcode.location + 2].text;
     const baseRes = barcode.result[0];
 
-    let reservations = [];
+    const isValidBaseRes = !baseRes.reservationFor.departureStation.name.match(/^\d+$/);
+    let reservations = isValidBaseRes ? [baseRes] : [];
     let idx = page.indexOf("Detaljer");
     while (true) {
         const leg = page.substr(idx).match(/(\S.*\S) +Afg: +(\d\d:\d\d)\n *(\S.*\S) +Ank: +(\d\d:\d\d) +(\S.*) til/);
         if (!leg)
             break;
         idx += leg.index + leg[0].length;
-        let res = JsonLd.clone(baseRes);
+        if (leg[5].match(/Gåtur|Walk/))
+            continue;
+        let res = isValidBaseRes ? JsonLd.newTrainReservation() : JsonLd.clone(baseRes);
         res.reservationFor.departureStation.name = leg[1];
-        res.reservationFor.departureTime = JsonLd.toDateTime(res.reservationFor.departureDay + leg[2], 'yyyy-MM-ddhh:mm', 'dk');
+        res.reservationFor.departureTime = JsonLd.toDateTime(baseRes.reservationFor.departureDay + leg[2], 'yyyy-MM-ddhh:mm', 'dk');
         res.reservationFor.arrivalStation.name = leg[3];
         res.reservationFor.arrivalTime =
-        JsonLd.toDateTime(res.reservationFor.departureDay + leg[4], 'yyyy-MM-ddhh:mm', 'dk');
+        JsonLd.toDateTime(baseRes.reservationFor.departureDay + leg[4], 'yyyy-MM-ddhh:mm', 'dk');
         res.reservationFor.trainNumber = leg[5];
         reservations.push(res);
     }
