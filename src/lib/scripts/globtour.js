53a3a2de3c5a77b96edf163a0a4e9cf8984a05c5
Volker Krause <vkrause@kde.org>
2025-07-22 17:34:11 +0200

    Add basic globtour.com bus ticket extractor script
    (cherry picked from commit 351d2a0fd30b29cb232a659d8e11b5233d6f13a6)

---
+++ src/lib/scripts/globtour.js
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: ⓒ 2025 Volker Krause <vkrause@kde.org>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+function extractTicket(pdf, node, barcode) {
+    const text = pdf.pages[barcode.location].text;
+    console.log(text);
+    let res = JsonLd.newBusReservation();
+    res.reservationNumber = text.match(/Booking Number (.*?)  /)[1];
+    res.reservationFor.provider.name = text.match(/operated by:\n(.*)/)[1];
+    const leg = text.match(/(\d{2}\. \S+\. \d{4}, \d\d:\d\d) *\| *(.*)\n.*\n *(\d{2}\. \S+\. \d{4}, \d\d:\d\d) *\| *(.*)\n/);
+    res.reservationFor.departureTime = JsonLd.toDateTime(leg[1], "dd. MMM. yyyy, HH:mm", "en");
+    res.reservationFor.departureBusStop.name = leg[2];
+    res.reservationFor.arrivalTime = JsonLd.toDateTime(leg[3], "dd. MMM. yyyy, HH:mm", "en");
+    res.reservationFor.arrivalBusStop.name = leg[4];
+    res.reservedTicket.ticketToken = 'qrcode:' + barcode.content;
+    return res;
+}
