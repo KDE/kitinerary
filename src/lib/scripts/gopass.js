448099ce967fe210787a497d56f6feb0d140cdf3
David Pilarčík <meow@charliecat.space>
2025-07-27 23:14:52 +0200

    Add Gopass Ticket Confirmation Extractor
    (cherry picked from commit fa59c0f49eb0ba7527ffe4a53a7408b333c6391f)

---
+++ src/lib/scripts/gopass.js
@@ -0,0 +1,77 @@
+function parsePdfConfirmation(pdf, node, triggerNode) {
+	const page = pdf.pages[triggerNode.location];
+	const text = page.text;
+	let res = JsonLd.newEventReservation();
+
+	// the "Potvrdenie o nákupe" string shows up in the header of the first page
+	let headerOffset = page.text.includes("Potvrdenie o nákupe") ? 0.06 : 0;
+	// Example: "Jan Novák\nŠtudent"
+	let [holderName, holderAgeGroup] = page.textInRect(0, headerOffset+0.11, 0.3, 0.25).split('\n');
+	res.underName.name = holderName.trim();
+	res.reservedTicket.name = holderAgeGroup.trim();
+
+	// For difffrent header sizes have to adjust the rectangle
+	res.reservationFor.name = page.textInRect(0, headerOffset, 0.5, 0.15).split(holderName)[0];
+
+	// Example: "Číslo lístku\n8f907e3b1928172c6e08baee36"
+	// Ticket number is diffrent pattern is diffrent for venue types.
+	// Tatralandia uses UUID-like ticker numbers
+	// Others use tickers like: "1-1760-66-205420"
+	res.reservationNumber = text.match(/Číslo lístku\s+(.*)\s+/)[1];
+
+	let date = page.textInRect(0.6, headerOffset, 1, 0.2).split('\n')[0].trim();
+	// Example: "23. 7. 2025" or "24. 3. 2023 - 27. 3. 2023"
+	if (date.includes(" - ")) {
+		let [startDate, endDate] = date.split(" - ");
+		res.reservationFor.startDate = JsonLd.toDateTime(startDate, 'd. M. yyyy', 'sk');
+		res.reservationFor.endDate = JsonLd.toDateTime(endDate, 'd. M. yyyy', 'sk');
+	} else {
+		res.reservationFor.startDate = JsonLd.toDateTime(date, 'd. M. yyyy', 'sk');
+	}
+
+	if (page.text.includes("PIN:")) {
+		// Extract the PIN from the text
+		// Ticket for establishments like Tatralandia or Besenova allow self-check-in with PIN from the ticket
+		// other tickets do not have a PIN. user checks in with their GoPass Card Code or QR code if gived
+		// Example: "PIN: 123456"
+		res.reservationFor.description = /PIN: (\d+) /.exec(page.text)?.[1];
+		res.reservedTicket.ticketNumber = res.description;
+	}
+
+	if (page.text.includes("QR kód ")) {
+		// Looking through the page for a QR code
+		// QR code never is the ticket number nor the GoPass Card Code
+		let qrCodeApplicable = node.childNodes
+			.filter(c => c.mimeType == 'internal/qimage')
+			.map(c => c.childNodes[0]?.content)
+			.filter(c => c != undefined && c.length >= 24 && c !== res.reservationNumber);
+		if (qrCodeApplicable.length != 0) res.reservedTicket.ticketToken = 'qrcode:' + qrCodeApplicable[triggerNode.location];
+	} else if (page.text.includes("Turniketom prejdete načítaním QR kódu.")) {
+		// If the ticket says to scan the QR code at the turnstile,
+		// and the ticket doesn't have a QR code,
+		// we can assume the QR code is the ticket number in a barcode format.
+		res.reservedTicket.ticketToken = 'code128:' + res.reservationNumber;
+	} else {
+		// At the end, we can assume the ticket, if needed to be scanned, is the the same code as GoPass Card Code
+		res.reservedTicket.ticketToken = 'code128:' + triggerNode.content;
+	}
+
+	// The Membership number is the GoPass Card Number
+	res.programMembershipUsed = { membershipNumber: triggerNode.content, programName: "Gopass" };
+
+	ExtractorEngine.extractPrice(res.reservedTicket, text);
+
+	// console.log(JSON.stringify(res, null, 4));
+	// Example: "Využitie na miestach:\n• Vodný park Tatralandia"
+	// Gopass allows for tickets to be used at multiple places.
+	// The places are listed in the ticket.
+	// Reason why? Gopass allows purchase of multiday skipasses for ski resorts,
+	// which use the same ticket PDF template
+	let placesApplicable = page.textInRect(0.3, headerOffset+0.11, 0.7, 0.4)
+		.split('\n')
+		.filter(line => line.includes('• '))
+		.map(line => line.replace('• ', '').trim());
+	res.reservationFor.location.name = placesApplicable.join(', ');
+
+	return res;
+}
