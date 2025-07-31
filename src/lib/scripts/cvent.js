ec4e4c11c2b0dfc5a78bbb6a01c596870ab82de1
David Pilarčík <meow@charliecat.space>
2025-07-31 08:50:02 +0200

    Add Cvent PkPass Extractor
    (cherry picked from commit 97bec2bb1fc3a61cf7809df38176fc6be365fd9a)

---
+++ src/lib/scripts/cvent.js
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2025 David Pilarčík <meow@charliecat.space>
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+function parsePkPass(pass, node) {
+    let res = node.result[0];
+	res.reservationFor.name = pass.field["title"].value;
+	res.reservedTicket.name = pass.field["ticket_type"].value;
+	res.reservationFor.startDate = pass.field["start_date"].value;
+	res.reservationFor.location = Object.assign(
+		JsonLd.newObject('Place'),
+		{
+			name: pass.field["location"].value,
+		}
+	)
+	res.underName = Object.assign(
+		JsonLd.newObject('Person'),
+		{
+			name: pass.field["attendee_name"]?.value
+		}
+	)
+	res.reservationNumber = pass.barcodes[0].alternativeText;
+
+	return res
+}
