02e3c3463a96b3a3aa46c1f6b2e4b106be6b2e79
Volker Krause <vkrause@kde.org>
2025-09-11 14:53:31 +0200

    Make Ryanair extractor more robust for extracting the upside down parts
    Some of their PDFs with an upside-down bottom half don't correctly rotate
    the text when doing regular text extraction, so rotate that explicitly.
    
    Also deal with documents without an arrival time.
    
    (cherry picked from commit a8bad5cc10d7b1fd11d5ba3be5074eab742c55e6)

--- src/lib/scripts/ryanair.js
+++ src/lib/scripts/ryanair.js
@@ -6,10 +6,13 @@
 function parseBoardingPass(pdf, node, triggerNode) {
     var res = triggerNode.result[0];
     const page = pdf.pages[triggerNode.location];
-    const timesText = page.textInRect(0.5, 0.5, 1, 1);
-    const times = timesText.match(/\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)/);
-    res.reservationFor.boardingTime = JsonLd.toDateTime(times[2], "hh:mm", "en");
-    res.reservationFor.departureTime = JsonLd.toDateTime(times[3], "hh:mm", "en");
-    res.reservationFor.arrivalTime = JsonLd.toDateTime(times[4], "hh:mm", "en");
+    const timesText = page.textInRect(0.0, 0.0, 0.5, 0.5, 180);
+    let times = timesText.match(/\n(?:\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)/);
+    if (!times) {
+        times = timesText.match(/\n(?:\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)/);
+    }
+    res.reservationFor.boardingTime = JsonLd.toDateTime(times[1], "hh:mm", "en");
+    res.reservationFor.departureTime = JsonLd.toDateTime(times[2], "hh:mm", "en");
+    res.reservationFor.arrivalTime = JsonLd.toDateTime(times[3], "hh:mm", "en");
     return res;
 }
