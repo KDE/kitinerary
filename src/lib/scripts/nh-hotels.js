0db0c865beb95745d1f43df8e0da7a42ff679ed2
Volker Krause <vkrause@kde.org>
2025-10-27 17:24:39 +0100

    Adapt NH Hotels extractor to newer variants

--- src/lib/scripts/nh-hotels.js
+++ src/lib/scripts/nh-hotels.js
@@ -16,36 +16,51 @@ function parseDateTime(text) {
     var dt = text.match(/(\d+\/\d+\/\d+).*?(\d+:\d+)/);
     if (dt)
         return JsonLd.toDateTime(dt[1] + dt[2], "dd/MM/yyyyhh:mm", "en");
-    return JsonLd.toDateTime(text.substr(3), "dd MMM yyyy hh:mm a", "en");
+    return JsonLd.toDateTime(text.substr(3).trim(), "dd MMM yyyy hh:mm a", "en");
+}
+
+function parseAddress(res, addrText)
+{
+    const addr = addrText.match(/(.*), ?([^,]*)/);
+    if (addr) {
+        res.reservationFor.address.streetAddress = addr[1];
+        res.reservationFor.address.addressLocality = addr[2];
+        return true;
+    }
+    return false;
 }
 
 function parseHtml(doc) {
+    const text = doc.root.recursiveContent;
+    let res = JsonLd.newLodgingReservation();
 
-    var res = JsonLd.newLodgingReservation();
-
-    var nameElem = doc.eval("//td[@class=\"title-hotel\" or @class=\"hotel-title\"]")[0];
-    res.reservationFor.name = nameElem.content;
+    let nameElem = doc.eval("//td[@class=\"title-hotel\" or @class=\"hotel-title\"]");
+    nameElem = nameElem.length > 0 ? nameElem[0] : undefined;
+    if (nameElem) {
+        res.reservationFor.name = nameElem.content;
+    } else {
+        res.reservationFor.name = text.match(/check-in\n(.*)\n/)[1];
+    }
 
     let addrElem = doc.eval("//td[@class=\"hotel-address\"]");
-    if (addrElem.length > 0) {
-        addrElem = addrElem[0];
-    } else {
+    addrElem = addrElem.length > 0 ? addrElem[0] : undefined;
+    if (!addrElem && nameElem) {
         addrElem = nameElem.parent.nextSibling.nextSibling.nextSibling.firstChild;
     }
-    let addr = addrElem.content.match(/(.*), ?([^,]*)/);
-    if (!addr)
-        addr = addrElem.recursiveContent.match(/(.*), ?([^,]*)/);
-    res.reservationFor.address.streetAddress = addr[1];
-    res.reservationFor.address.addressLocality = addr[2];
-    res.reservationFor.url = lastChild(addrElem).attribute("href");
+    if (addrElem) {
+        parseAddress(res, addrElem.content) || parseAddress(res, addrElem.recursiveContent);
+        res.reservationFor.url = lastChild(addrElem).attribute("href");
+    } else {
+        parseAddress(res, text.match(/check-in\n.*\n(.*)\n/)[1]);
+    }
 
-    var linkRoot = addrElem.parent.parent;
+    var linkRoot = addrElem ? addrElem.parent.parent : doc.root;
     var links = linkRoot.eval(".//a");
     for (var i = 0; i < links.length; ++i) {
         var url = links[i].attribute("href");
         if (url.startsWith("tel:"))
             res.reservationFor.telephone = url.substr(4);
-        else if (url.startsWith("mailto:"))
+        else if (url.startsWith("mailto:") && !res.reservationFor.email)
             res.reservationFor.email = url.substr(7);
     }
 
@@ -72,8 +87,7 @@ function parseHtml(doc) {
             res.checkinTime = parseDateTime(dts[0].content);
             res.checkoutTime = parseDateTime(dts[1].content);
         } else {
-            const text = doc.root.recursiveContent;
-            res.reservationNumber = text.match(/(?:Reservation number|Reservierungsnummer):?[\n ](\d+)/)[1];
+            res.reservationNumber = text.match(/(?:Reservation number|Reservierungsnummer|reservation number is ):?[\n ]?(\d+)/)[1];
             res.checkinTime = parseDateTime(text.match(/(?:Check-in|Anreise):[\n ](.*)/i)[1]);
             res.checkoutTime = parseDateTime(text.match(/(?:Check-out|Abreise):[\n ](.*)/i)[1]);
         }
