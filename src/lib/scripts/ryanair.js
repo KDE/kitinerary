628060688f45f5d855ebb87b017722187f7b66be
Volker Krause <vkrause@kde.org>
2025-10-28 17:26:10 +0100

    Make RyanAir times parsing more robust
    This now works with more variants of the timeline on the lower half of
    the boarding pass.
    
    (cherry picked from commit 66e09fdbdc03e4f1780862ae741dfb73b011a78b)

--- src/lib/scripts/ryanair.js
+++ src/lib/scripts/ryanair.js
@@ -4,16 +4,19 @@
 */
 
 function parseBoardingPass(pdf, node, triggerNode) {
-    var res = triggerNode.result[0];
     const page = pdf.pages[triggerNode.location];
-    const timesText = page.textInRect(0.0, 0.0, 0.5, 0.5, 180);
-    let times = timesText.match(/\n(?:\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)/);
-    if (!times) {
-        times = timesText.match(/\n(?:\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)[\s\S]*?\n(\d\d:\d\d)/);
-    }
-    res.reservationFor.boardingTime = JsonLd.toDateTime(times[1], "hh:mm", "en");
-    res.reservationFor.departureTime = JsonLd.toDateTime(times[2], "hh:mm", "en");
-    res.reservationFor.arrivalTime = JsonLd.toDateTime(times[3], "hh:mm", "en");
+    let res = triggerNode.result[0];
+
+    const topRight = page.textInRect(0.5, 0.0, 1.0, 0.5);
+    const depTimes = topRight.match(/(\d\d:\d\d).*(\d\d:\d\d)/);
+    res.reservationFor.boardingTime = JsonLd.toDateTime(depTimes[1], "hh:mm", "en");
+    res.reservationFor.departureTime = JsonLd.toDateTime(depTimes[2], "hh:mm", "en");
+
+    const bottomRight = page.textInRect(0.0, 0.0, 0.5, 0.5, 180);
+    const times = bottomRight.match(/(\d\d:\d\d)/g);
+    if (times.length > 3 && times[times.length - 1] != depTimes[2])
+        res.reservationFor.arrivalTime = JsonLd.toDateTime(times[times.length -1], "hh:mm", "en");
+
     return res;
 }
 
