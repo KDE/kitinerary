66f68ad8068a23aa67a2f92fe7ea1b6454847799
Tobias Fella <tobias.fella@kde.org>
2026-01-02 01:38:06 +0100

    Add support for gomus annual tickets
    (cherry picked from commit 004ca5556c589c7a7cf77656460c33ef70dd2dbf)

--- src/lib/scripts/gomus.js
+++ src/lib/scripts/gomus.js
@@ -5,10 +5,30 @@
 
 function parsePass(pass, node) {
     var event = node.result[0];
-    event.reservationFor.name = pass.field['event-name'].value;
-    event.reservationFor.location.name = pass.field['location'].value;
-    event.reservationFor.location.address = JsonLd.newObject('PostalAddress');
-    event.reservationFor.location.address.streetAddress = pass.locations[0].relevantText.match(/ in (.*)\.$/)[1];
-    event.reservedTicket.name = pass.description;
-    return event;
+
+    if (pass.field['event-name'].label !== "Jahreskarte") {
+        event.reservedTicket.name = pass.description;
+        event.reservationFor.name = pass.field['event-name'].value;
+        event.reservationFor.location.name = pass.field['location'].value;
+        event.reservationFor.location.address = JsonLd.newObject('PostalAddress');
+        event.reservationFor.location.address.streetAddress = pass.locations[0].relevantText.match(/ in (.*)\.$/)[1];
+        return event;
+    }
+
+    var ticket = JsonLd.newObject("Ticket")
+    for (const field of pass.secondaryFields) {
+        if (field.label === "gültig ab") {
+            ticket.validFrom = field.value;
+            break;
+        }
+    }
+    ticket.ticketNumber = pass.field['numbering'].value;
+    ticket.validUntil = pass.expirationDate;
+    ticket.issuedBy = JsonLd.newObject("Organization");
+    ticket.issuedBy.name = pass.organizationName;
+    ticket.name = pass.field['event-name'].value + " - " + pass.organizationName;
+    ticket.ticketToken = event.reservedTicket.ticketToken;
+    ticket.underName = JsonLd.newObject("Person");
+    ticket.underName.name = pass.field['personalization'].value;
+    return ticket;
 }
