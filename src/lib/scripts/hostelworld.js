8837a4d290c39096bed4852af2273229764d4cf9
Carl Schwan <carl@carlschwan.eu>
2026-02-22 19:52:47 +0100

    hostelworld: Add support for new email format
    Fortunately this seems to be coming from a seperate domain so easy to
    differentiate the old and new format.
    
    
    (cherry picked from commit 95e77d8658857ae69bf3ca312bf7bdbb2c7458d3)
    
    Co-authored-by: Carl Schwan <carlschwan@kde.org>

--- src/lib/scripts/hostelworld.js
+++ src/lib/scripts/hostelworld.js
@@ -25,3 +25,58 @@ function parseReservationMail(content) {
 
     return res;
 }
+
+function parseReservationMailV2(content) {
+    let res = JsonLd.newLodgingReservation();
+
+    const lines = content.split('\n');
+    let dateStart;
+    let dateEnd;
+    for (let i = 0, count = lines.length; i < count; i++) {
+        const line = lines[i];
+
+        if (line.startsWith('Booking Reference')) {
+            res.reservationNumber = lines[i + 1];
+
+            const name = lines[i + 2].split(',');
+            res.reservationFor.name = name[0]
+
+            const address = lines[i + 3].split(',');
+            res.reservationFor.address.streetAddress = address[0];
+            res.reservationFor.address.addressLocality = address[address.length - 2];
+            res.reservationFor.address.addressCountry = address[address.length - 1];
+
+            const dates = lines[i + 4].split(',')[0].split(' - ');
+
+            dateStart = dates[0]
+            dateEnd = dates[1]
+        }
+
+        if (line.startsWith('Total')) {
+            const match = line.match("Total ([A-Z]{3}) (.*)");
+            if (match) {
+                res.totalPrice = match[2];
+                res.priceCurrency = match[1];
+            }
+        }
+
+        if (line.startsWith('Check-In:')) {
+            const match = line.match("From ([0-9]{2}:[0-9]{2})");
+            if (match) {
+                dateStart += ' ' + match[1];
+            }
+        }
+
+        if (line.startsWith('Check-Out:')) {
+            const match = line.match("Before ([0-9]{2}:[0-9]{2})");
+            if (match) {
+                dateEnd += ' ' + match[1];
+            }
+        }
+    }
+
+    res.checkinTime = JsonLd.toDateTime(dateStart, ["d MMM yyyy hh:mm", "d MMM yyyy"], ["en","de"]);
+    res.checkoutTime = JsonLd.toDateTime(dateEnd, ["d MMM yyyy hh:mm", "d MMM yyyy"], ["en","de"]);
+
+    return res;
+}
