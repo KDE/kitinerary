02ecce2bd5f981133643d394f84b61393b03fb97
Ajinkya Dahale <dahale.a.p@gmail.com>
2026-06-02 20:32:36 +0530

    Extract more data from ktmb pdf
    Does not seem to work perfectly yet.
    1. This does not support Shuttle Tebrau between Johor Bahru and Singapore (or
    theoretically other tickets without a seat). That shuttle itself is planned to
    be replaced by a metro.
    2. The ticket version (downloaded from the KTMB ticketing website) is different
    than the PDF attached with the email. Incorporating that will need using
    rectangles.
    3. There is a complex case where the middle data (name, ticket number, etc) are
    stored in a format that is neither detected as text nor image in kitinerary. I
    don't recall how I came across this version of the ticket.

--- src/lib/scripts/ktmb.js
+++ src/lib/scripts/ktmb.js
@@ -8,18 +8,18 @@ function extractQrTicket(text) {
 
     res.reservedTicket.ticketToken = 'qrcode:' + text;
 
-    var div = text.match(/([0-9]*),[a-f0-9-]{36},(T[0-9]{12}),([^,]*),([^,]*),([^,]*),[^,]*,[^,]*,(.+)/);
+    var extrData = text.match(/([0-9]*),[a-f0-9-]{36},(T[0-9]{12}),([^,]*),([^,]*),([^,]*),[^,]*,[^,]*,(.+)/);
 
-    if (parseInt(div[1]) != 1) {
+    if (parseInt(extrData[1]) != 1) {
         // TODO: multi passenger support?
         return [];
     }
 
-    res.reservationNumber = div[2];
-    res.reservationFor.trainNumber = div[3];
-    res.reservationFor.departureTime = JsonLd.toDateTime(div[6], "dd MMM yyyy hh:mm:ss ap", "en");
-    res.reservedTicket.ticketedSeat.seatSection = div[4];
-    res.reservedTicket.ticketedSeat.seatNumber = div[5];
+    res.reservationNumber = extrData[2];
+    res.reservationFor.trainNumber = extrData[3];
+    res.reservationFor.departureTime = JsonLd.toDateTime(extrData[6], "dd MMM yyyy hh:mm:ss ap", "en");
+    res.reservedTicket.ticketedSeat.seatSection = extrData[4];
+    res.reservedTicket.ticketedSeat.seatNumber = extrData[5];
 
     return res;
 }
@@ -28,11 +28,25 @@ function extractPdfTicketWithQr(pdf, node, triggerNode) {
     let res = triggerNode.result;
     const text = pdf.pages[triggerNode.location].text;
 
-    // FIXME: not working
+    var extrData = text.match(/\s+(.+) - (.+)\n\s*Berlepas \/ Departure :\s*Tiba \/ Arrival :\n\n\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\n\n\s+(.+)\n\s+(.+) - Adult\n\s+(\S+)\s+Coach\/Seat\n\s+MYR ([0-9.]+) \/ .+\S\s\s+(.+)\/(.+)\n\s+(K\d{12})\*\*(T\d{12}) \(Single\)\s+Online\s+(.+)/);
+
     // TODO: not supposed to be there for just "adults"
     // This may include other classes such as "senior" or "student"
-    for (let r of res)
-        r.underName.name = text.match(/(.+) - Adult/)[1];
+    for (let r of res) {
+        r.reservationFor.departureStation.name = extrData[1];
+        r.reservationFor.arrivalStation.name = extrData[2];
+        r.reservationFor.departureTime = JsonLd.toDateTime(extrData[3], "dd/MM/yyyy hh:mm ap", "en");
+        r.reservationFor.arrivalTime = JsonLd.toDateTime(extrData[4], "dd/MM/yyyy hh:mm ap", "en");
+        r.reservationFor.trainName = extrData[5];
+        r.underName.name = extrData[6];
+        // r.underName.identifier = extrData[7]; // FIXME: Confirm
+        r.totalPrice = parseFloat(extrData[8]);
+        r.priceCurrency = 'MYR';
+        r.reservedTicket.ticketedSeat.seatSection = extrData[9];
+        r.reservedTicket.ticketedSeat.seatNumber = extrData[10];
+        // There are two numbers: booking, which is common for all, and ticket, unique for each passenger
+        r.reservationNumber = extrData[12];
+    }
 
     return res;
 }
@@ -40,10 +54,23 @@ function extractPdfTicketWithQr(pdf, node, triggerNode) {
 function extractTextTicket(text) {
     let res = JsonLd.newTrainReservation();
 
-    // FIXME: not working
     // TODO: not supposed to be there for just "adults"
     // This may include other classes such as "senior" or "student"
-    res.underName.name = text.match(/(.+) - Adult/)[1];
+    var extrData = text.match(/\s+(.+) - (.+)\n\s*Berlepas \/ Departure :\s*Tiba \/ Arrival :\n\n\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\n\n\s+(.+)\n\s+(.+) - Adult\n\s+(\S+)\s+Coach\/Seat\n\s+MYR ([0-9.]+) \/ .+\S\s\s+(.+)\/(.+)\n\s+(K\d{12})\*\*(T\d{12}) \(Single\)\s+Online\s+(.+)/);
+
+    res.reservationFor.departureStation.name = extrData[1];
+    res.reservationFor.arrivalStation.name = extrData[2];
+    res.reservationFor.departureTime = JsonLd.toDateTime(extrData[3], "dd/MM/yyyy hh:mm ap", "en");
+    res.reservationFor.arrivalTime = JsonLd.toDateTime(extrData[4], "dd/MM/yyyy hh:mm ap", "en");
+    res.reservationFor.trainName = extrData[5];
+    res.underName.name = extrData[6];
+    // res.underName.identifier = extrData[7]; // FIXME: Confirm
+    res.totalPrice = parseFloat(extrData[8]);
+    res.priceCurrency = 'MYR';
+    res.reservedTicket.ticketedSeat.seatSection = extrData[9];
+    res.reservedTicket.ticketedSeat.seatNumber = extrData[10];
+    // There are two numbers: booking, which is common for all, and ticket, unique for each passenger
+    res.reservationNumber = extrData[12];
 
     return res;
 }
@@ -52,11 +79,25 @@ function extractPdfTicketWithoutQr(pdf, node, triggerNode) {
     let res = triggerNode.result;
     const text = pdf.pages[triggerNode.location].text;
 
-    // FIXME: not working
+    var extrData = text.match(/\s+(.+) - (.+)\n\s*Berlepas \/ Departure :\s*Tiba \/ Arrival :\n\n\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\s+(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2} [AP]M)\n\n\s+(.+)\n\s+(.+) - Adult\n\s+(\S+)\s+Coach\/Seat\n\s+MYR ([0-9.]+) \/ .+\S\s\s+(.+)\/(.+)\n\s+(K\d{12})\*\*(T\d{12}) \(Single\)\s+Online\s+(.+)/);
+
     // TODO: not supposed to be there for just "adults"
     // This may include other classes such as "senior" or "student"
-    for (let r of res)
-        r.underName.name = text.match(/(.+) - Adult/)[1];
+    for (let r of res) {
+        r.reservationFor.departureStation.name = extrData[1];
+        r.reservationFor.arrivalStation.name = extrData[2];
+        r.reservationFor.departureTime = JsonLd.toDateTime(extrData[3], "dd/MM/yyyy hh:mm ap", "en");
+        r.reservationFor.arrivalTime = JsonLd.toDateTime(extrData[4], "dd/MM/yyyy hh:mm ap", "en");
+        r.reservationFor.trainName = extrData[5];
+        r.underName.name = extrData[6];
+        // r.underName.identifier = extrData[7]; // FIXME: Confirm
+        r.totalPrice = parseFloat(extrData[8]);
+        r.priceCurrency = 'MYR';
+        r.reservedTicket.ticketedSeat.seatSection = extrData[9];
+        r.reservedTicket.ticketedSeat.seatNumber = extrData[10];
+        // There are two numbers: booking, which is common for all, and ticket, unique for each passenger
+        r.reservationNumber = extrData[12];
+    }
 
     return res;
 }
