7158b4e7a68c12ebad25bebfd3b209fba38c1402
Volker Krause <vkrause@kde.org>
2026-06-11 17:38:43 +0200

    Extract checkin times and prices from Accor hotel bookings
    (cherry picked from commit 27fb52b9b24bc5ebfea6e43c0cdc4d93afdf011a)

--- src/lib/scripts/accor.js
+++ src/lib/scripts/accor.js
@@ -4,20 +4,26 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
-function parseConfirmation(html) {
-    var res = JsonLd.newLodgingReservation();
+function parseConfirmation(html, node) {
+    let res = node.result.length === 1 ? node.result[0] : JsonLd.newLodgingReservation();
 
     const content = html.root.recursiveContent;
-    res.reservationNumber = content.match(/(?:Reservation number|Buchungsnummer):?\s*([0-9A-Z]+)/)[1];
+    res.reservationNumber = content.match(/(?:Reservation number|Reservation n°|Reservierung Nr.|Buchungsnummer):?\s*([0-9A-Z]+)/)[1];
 
     let dt = content.match(/(\d{2}[|/.]\d{2}[|/.]\d{4}).*(\d{2}[|/.]\d{2}[|/.]\d{4})/);
     if (!dt) // US format
         dt = content.match(/([A-Z][a-z]{2}\. \d{2}, \d{4}).*([A-Z][a-z]{2}\. \d{2}, \d{4})/);
     if (!dt)
         dt = content.match(/(\d{2} [A-Z][a-z]{2} \d{4}).*(\d{2} [A-Z][a-z]{2} \d{4})/);
-    const formats = [ "dd.MM.yyyy", "dd/MM/yyyy", "MMM. dd, yyyy", "dd MMM yyyy"];
-    res.checkinTime = JsonLd.toDateTime(dt[1], formats, "en");
-    res.checkoutTime = JsonLd.toDateTime(dt[2], formats, "en");
+    const times = content.match(/(?:Check in Policy|Anreisezeit):?\n.*(\d\d:\d\d).*\n(?:Check out Policy|Abreisezeit):?\n.*(\d\d:\d\d)/);
+    const formats = [
+        "dd.MM.yyyyHH:mm", "dd.MM.yyyy",
+        "dd/MM/yyyyHH:mm", "dd/MM/yyyy",
+        "MMM. dd, yyyyHH:mm", "MMM. dd, yyyy",
+        "dd MMM yyyyHH:mm", "dd MMM yyyy"
+    ];
+    res.checkinTime = JsonLd.toDateTime(dt[1] + (times ? times[1] : ""), formats, "en");
+    res.checkoutTime = JsonLd.toDateTime(dt[2] + (times ? times[2] : ""), formats, "en");
 
     let hotelNode = html.eval('//table//table//table[@class="table-full"]');
     if (!hotelNode || hotelNode.length === 0)
@@ -33,18 +39,24 @@ function parseConfirmation(html) {
         // no contact information present
         hotel = hotelContent.match(/^(.*)\n+(.*\n.*)\n(?:.|\n)*/);
     }
-    res.reservationFor.name = hotel[1];
 
+    if (hotel) {
+        res.reservationFor.name = hotel[1];
 
-    var addr = hotel[2].match(/(.*)(?: - |\n)(.*) - (.*)/);
-    res.reservationFor.address.streetAddress = addr[1];
-    res.reservationFor.address.addressLocality = addr[2];
-    res.reservationFor.address.addressCountry = addr[3];
+        const addr = hotel[2].match(/(.*)(?: - |\n)(.*) - (.*)/);
+        res.reservationFor.address.streetAddress = addr[1];
+        res.reservationFor.address.addressLocality = addr[2];
+        res.reservationFor.address.addressCountry = addr[3];
+    }
 
     // this is in an HTML comment...
     if (html.rawData.match(/TARS - AH - Client - Cancellation - Email/)) {
         res.reservationStatus = "ReservationCancelled"
     }
 
+    ExtractorEngine.extractPrice(content, res);
+    const price = content.match(/(?:Total|Gesamt).*\n(\s*\n)?.*\d.*\n/);
+    if (price && !res.totalPrice)
+        ExtractorEngine.extractPrice(price[0], res);
     return res;
 }
