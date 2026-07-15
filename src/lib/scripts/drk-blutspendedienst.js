eb7f5626f9204058c9454a48cfcaa3ceb5858a47
Volker Krause <vkrause@kde.org>
2026-07-15 19:15:42 +0200

    Adapt DRK extractor to email layout changes
    (cherry picked from commit 71fc2da82eb7d9828288037ffb3f4307befbb3bb)

--- src/lib/scripts/drk-blutspendedienst.js
+++ src/lib/scripts/drk-blutspendedienst.js
@@ -4,19 +4,29 @@
 */
 
 function parseReservation(html) {
-    var h4s = html.eval('//h4');
-    var res = JsonLd.newEventReservation();
-    res.reservationFor.name = h4s[0].content;
-    var dt = h4s[1].content.match(/(\d+\.\d+\.\d+) - (\d+:\d+)/);
-    res.reservationFor.startDate = JsonLd.toDateTime(dt[1] + dt[2], "dd.MM.yyyyhh:mm", "de");
-    res.reservationFor.location.name = h4s[2].content;
-    var addr = h4s[2].nextSibling.content.match(/(.*)\n+(.*)/);
-    res.reservationFor.location.address.streetAddress = addr[1];
-    res.reservationFor.location.address.addressLocality = addr[2];
+    let res = JsonLd.newEventReservation();
     res.reservationFor.location.address.addressCountry = 'DE';
 
-    var links = html.eval('//a');
-    res.modifyReservationUrl = links[links.length - 1].attribute('href');
+    const h4s = html.eval('//h4');
+    if (h4s.length > 0) {
+        res.reservationFor.name = h4s[0].content;
+        var dt = h4s[1].content.match(/(\d+\.\d+\.\d+) - (\d+:\d+)/);
+        res.reservationFor.startDate = JsonLd.toDateTime(dt[1] + dt[2], "dd.MM.yyyyhh:mm", "de");
+        res.reservationFor.location.name = h4s[2].content;
+        var addr = h4s[2].nextSibling.content.match(/(.*)\n+(.*)/);
+        res.reservationFor.location.address.streetAddress = addr[1];
+        res.reservationFor.location.address.addressLocality = addr[2];
 
+    } else {
+        const text = html.root.recursiveContent;
+        const ev = text.match(/\| (\d{2}\.\d{2}\.\d{4}).*(\d\d:\d\d).*\n(.*)\n(.*)\n(.*)\n(.*)/);
+        res.reservationFor.startDate = JsonLd.toDateTime(ev[1] + ev[2], "dd.MM.yyyyHH:mm", "de");
+        res.reservationFor.name = ev[3];
+        res.reservationFor.location.name = ev[5];
+        res.reservationFor.location.address.addressLocality = ev[4];
+        res.reservationFor.location.address.streetAddress = ev[6];
+    }
+    const links = html.eval('//a');
+    res.modifyReservationUrl = links[links.length - 1].attribute('href');
     return res;
 }
